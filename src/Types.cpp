5dfc15badea9a0a7e078619fdc0adc146c9a8e0c
Fredrik Kjolstad <fredrikbk@gmail.com>
2014-07-15 01:05:09 -0400

    Adds a partial dimension implementation
    Without set dimensions

--- src/Types.cpp
+++ src/Types.cpp
@@ -1,1 +1,30 @@
 #include "Types.h"
+#include <iostream>
+#include <assert.h>
+
+using namespace Simit;
+using namespace std;
+
+
+// Dimension
+Dimension::Dimension()
+: type(VARIABLE) {}
+
+Dimension::Dimension(unsigned int size)
+: type(ANONYMOUS), size(size) {}
+
+Dimension::~Dimension() {}
+
+Dimension::operator std::string() const {
+  switch(type) {
+    case VARIABLE:
+      return "*";
+    case ANONYMOUS:
+      return to_string(size);
+    case SET:
+      assert(false); // Not supported yet
+  }
+
+  assert(false);
+  return "";
+}
