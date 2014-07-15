151adac9b33de884a82b302966afc56d8d51a9cb
Fredrik Kjolstad <fredrikbk@gmail.com>
2014-07-15 00:09:58 -0400

    Adds %code requires segment to parser
    This ensures header files that define types that are part of YYLTYPE
    gets inserted into the parser header file.

--- src/Frontend.h
+++ src/Frontend.h
@@ -5,16 +5,14 @@
 
 namespace Simit {
   class Program;
-  class Test;
-  class IRNode;
 
   class Frontend {
   public:
     Frontend();
     ~Frontend();
 
-    int parseString(std::string programString, Simit::Program *program);
-    int parseFile(std::string filename, Simit::Program *program);
+    int parseString(std::string programString, Program *program);
+    int parseFile(std::string filename, Program *program);
 
   private:
 
