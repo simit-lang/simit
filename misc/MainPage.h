4ff75462a1938dd0be57cba189669daa31e2a0c4
Fredrik Kjolstad <fred@csail.mit.edu>
2014-07-17 16:14:51 -0400

    Adds Doxyfile

---
+++ misc/MainPage.h
@@ -0,0 +1,20 @@
+/** \file
+ * This file only exists to contain the front-page of the documentation
+ */
+
+/** \mainpage Simit
+ *
+ * Simit is a programming language designed to make it easier to write
+ * high-performance code to simulate sparse systems.
+ *
+ * To learn Simit, we recommend you start by looking at our <a href=examples.html>examples</a>.
+ *
+ * You can also look in the test/input folder for many small examples
+ * that use various Simit features.
+ *
+ */
+
+/**
+ * \example examples/springs.sim
+ * \example examples/rigid.sim
+ */
