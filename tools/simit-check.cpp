df19c75ef3789e9e85570629fcde347ebddebe3e
Fredrik Kjolstad <fred@csail.mit.edu>
2014-08-10 02:00:56 -0400

    Cleans up simit-check

--- tools/simit-check.cpp
+++ tools/simit-check.cpp
@@ -1,8 +1,5 @@
 #include "program.h"
-using namespace simit;
-
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 int main(int argc, const char* argv[]) {
@@ -11,15 +8,12 @@ int main(int argc, const char* argv[]) {
     return 3;
   }
 
-  Program program;
-  const char *filename = argv[1];
-  int status = program.loadFile(filename);
-
+  simit::Program program;
+  int status = program.loadFile(argv[1]);
   if (status == 2) {
     cerr << "Error: Could not open file" << endl;
     return 2;
   }
-
   else if (status != 0) {
     cerr << "Error: Could not parse program" << endl;
     cerr << program.getErrorString() << endl;
