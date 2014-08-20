cde528bdb041c6e378a171b83910d21cb3ea8cd6
Fredrik Kjolstad <fred@csail.mit.edu>
2014-08-20 14:55:42 -0400

    Cleaning up the simit-check output

--- tools/simit-check.cpp
+++ tools/simit-check.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int main(int argc, const char* argv[]) {
   if (argc != 2) {
-    cerr << "Usage: SimitRun <simit-source>" << endl;
+    cerr << "Usage: simit-check <simit-source>" << endl;
     return 3;
   }
 
   simit::Program program;
   int status = program.loadFile(argv[1]);
   if (status == 2) {
-    cerr << "Error: Could not open file" << endl;
+    cerr << "Error opening file" << endl;
     return 2;
   }
   else if (status != 0) {
-    cerr << "Error: Could not parse program" << endl;
+    cerr << "Error parsing the program" << endl;
     cerr << program.getErrorString() << endl;
     return 1;
   }
 
   if (program.verify() != 0) {
-    cerr << "Error: Test failure" << endl;
+    cerr << "Error while running test" << endl;
     cerr << program.getErrorString() << endl;
     return 4;
   }
