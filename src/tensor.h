eb78aeef3e99509fb61e38e5b21f60b514583a5e
Fredrik Kjolstad <fred@csail.mit.edu>
2014-09-04 04:01:30 -0400

    More tensor code
    Adds assignment and reads

--- src/tensor.h
+++ src/tensor.h
@@ -21,76 +21,59 @@ inline Type typeOf<int>() { return Type::INT; }
 
 /// A tensor is a generalization of scalars, vectors and matrices. Tensors can
 /// be fields of \ref Set instances and can be bound to \ref Program instances.
-template <Type type, size_t order>
+template <typename T, size_t order>
 class TensorBase {
  public:
+  inline Type getType() { typeOf<T>(); }
+
   inline size_t getOrder() const { return order; }
+  inline size_t getDimension(size_t i) { assert(i < order); return dims[i]; }
 
-  size_t getDimension(size_t i) { assert(i < order); return dims[i]; }
-
- protected:
-  TensorBase() {}
-  ~TensorBase() {}
-
-  size_t dims[order];
-
-  void *data;
-  int dataSize;
-
-  void initData() {}
-};
-
-template <>
-class TensorBase<INT, 0> {
- public:
-  inline size_t getOrder() const { return 0; }
 
  protected:
-  TensorBase() {}
-  ~TensorBase() {}
-
-  int data;
-};
-
-template <>
-class TensorBase<FLOAT, 0> {
- public:
-  inline size_t getOrder() const { return 0; }
-
- protected:
-  TensorBase() {}
-  ~TensorBase() {}
-
-  double data;
-};
-
-template <>
-class TensorBase<ELEMENT, 0> {
- public:
-  inline size_t getOrder() const { return 0; }
-
- protected:
-  TensorBase() {
-    // TODO: Init element data
-  }
-  ~TensorBase() {}
-
-  void *data;
-  int dataSize;
-};
-
-template <Type type, size_t order>
-class Tensor : public TensorBase<type, order> {
- public:
-  Tensor() : TensorBase<type, order>() {}
+  inline TensorBase() {}
+  inline TensorBase(size_t d0) : dims{d0} { init(); }
+  inline TensorBase(size_t d0, size_t d1) : dims{d0,d1} { init(); }
+  inline TensorBase(size_t d0, size_t d1, size_t d2) : dims{d0,d1,d2} { init();}
 
   template<typename... Dims>
-  Tensor(Dims... dims) : TensorBase<type, order>() {
-    initDims(0, dims...);
-    this->initData();
+  inline TensorBase(Dims... dims) { initDims(0u, dims...); init(); };
+
+  ~TensorBase() { free(data); }
+
+  inline T &get(size_t i) {
+    return static_cast<T*>(this->data)[i];
+  }
+
+  inline T &get(size_t i, size_t j) {
+    assert(i < this->dims[0] && j < this->dims[1]);
+    return static_cast<T*>(this->data)[i*this->dims[1] + j];
+  }
+
+  inline T &get(size_t i, size_t j, size_t k) {
+    assert(i < this->dims[0] && j < this->dims[1] && k < this->dims[2]);
+    return static_cast<T*>(this->data)[i*this->dims[1]*this->dims[2] +
+                                       j*this->dims[2] + k];
+  }
+
+  template<typename... Indices>
+  inline T &get(Indices... indices) {
+    return static_cast<T*>(this->data)[computeOffset(0, indices...)];
   }
 
  private:
+  void *data;
+  Type type;
+  size_t dims[order];
+
+  void init() {
+    size_t numElements = 1;
+    for (size_t i=0; i<order; ++i) {
+      numElements *= dims[i];
+    }
+    data = malloc(numElements * sizeof(T));
+  }
+
   template<typename... Dims>
   void initDims(size_t i, size_t d, Dims... dims) {
     this->dims[i] = d;
@@ -101,54 +84,128 @@ class Tensor : public TensorBase<type, order> {
   void initDims(size_t i, size_t d) {
     this->dims[i] = d;
   }
+
+  template<typename... Indices>
+  inline size_t computeOffset(size_t i, size_t idx) {
+    assert(idx < dims[i]);
+    return i;
+  }
+
+  template<typename... Indices>
+  inline size_t computeOffset(size_t i, size_t idx, Indices... indices) {
+    assert(idx < dims[i]);
+    size_t stride = 1;
+    for (size_t j=i+1; j<order; ++j) {
+      stride *= dims[j];
+    }
+    return stride*idx + computeOffset(i+1, indices...);
+  }
 };
 
-template <Type type>
-class Tensor<type, 0> : public TensorBase<type, 0> {
+
+template <typename T>
+class TensorBase<T, 0> {
+ public:
+  inline size_t getOrder() const { return 0; }
+
+ protected:
+  TensorBase() {}
+  ~TensorBase() {}
+
+  T val;
+};
+
+template <typename T, size_t order>
+class Tensor : public TensorBase<T, order> {
+ public:
+  template<typename... Dims>
+  Tensor(Dims... dims) : TensorBase<T, order>(dims...) {}
+
+  template<typename... Indices>
+  inline T &operator()(Indices... indices) {
+    return this->get(indices...);
+  }
+
+  template<typename... Indices>
+  inline const T &operator()(Indices... indices) const {
+    return this->get(indices...);
+  }
+  
+ private:
+
+};
+
+template <typename T>
+class Tensor<T,0> : public TensorBase<T,0> {
  public:
   explicit Tensor() {}
+  Tensor(T val) { this->val = val; }
+
+  inline void operator= (const T &val) { this->val = val; }
+  inline operator T() const { return this->val; }
 };
 
-template <Type type>
-class Tensor<type, 1> : public TensorBase<type, 1> {
+template <typename T>
+class Tensor<T,1> : public TensorBase<T,1> {
  public:
-  explicit Tensor(size_t d0) {
-    this->dims[0] = d0;
-    this->initData();
+  explicit Tensor(size_t d0) : TensorBase<T,1>(d0) {}
+
+  inline T &operator()(size_t i) {
+    return this->get(i);
+  }
+
+  inline const T &operator()(size_t i) const {
+    return operator()(i);
   }
 };
 
-template <Type type>
-class Tensor<type, 2> : public TensorBase<type, 2> {
+template <typename T>
+class Tensor<T, 2> : public TensorBase<T,2> {
  public:
-  explicit Tensor(size_t d0, size_t d1) {
-    this->dims[0] = d0; this->dims[1] = d1;
-    this->initData();
+  explicit Tensor(size_t d0, size_t d1) : TensorBase<T,2>(d0,d1) {}
+
+  inline T &operator()(size_t i, size_t j) {
+    return this->get(i,j);
+  }
+
+  inline const T &operator()(size_t i, size_t j) const {
+    return this->get(i,j);
   }
 };
 
-template <Type type>
-class Tensor<type, 3> : public TensorBase<type, 3> {
+template <typename T>
+class Tensor<T, 3> : public TensorBase<T, 3> {
  public:
-  explicit Tensor(size_t d0, size_t d1, size_t d2) {
-    this->dims[0] = d0; this->dims[1] = d1; this->dims[2] = d2;
-    this->initData();
+  explicit Tensor(size_t d0, size_t d1, size_t d2) : TensorBase<T,3>(d0,d1,d2){}
+
+  inline T &operator()(size_t i, size_t j, size_t k) {
+    return this->get(i,j,k);
+  }
+
+  inline const T &operator()(size_t i, size_t j, size_t k) const {
+    return this->get(i,j,k);
   }
 };
 
-template <Type type> class Scalar : public Tensor<type, 0> {
+template <typename T>
+class Scalar : public Tensor<T, 0> {
  public:
-  explicit Scalar() {}
+  Scalar() {}
+  Scalar(T val) : Tensor<T, 0>(val) {}
+
+  inline void operator= (const T &val) { Tensor<T, 0>::operator=(val); }
 };
 
-template <Type type> class Vector : public Tensor<type, 1> {
+template <typename T>
+class Vector : public Tensor<T, 1> {
  public:
-  explicit Vector(size_t d0) : Tensor<type, 1>(d0) {}
+  explicit Vector(size_t d0) : Tensor<T, 1>(d0) {}
 };
 
-template <Type type> class Matrix : public Tensor<type, 2> {
+template <typename T>
+class Matrix : public Tensor<T, 2> {
  public:
-  explicit Matrix(size_t d0, size_t d1) : Tensor<type, 2>(d0, d1) {}
+  explicit Matrix(size_t d0, size_t d1) : Tensor<T, 2>(d0, d1) {}
 };
 
 
--- test/tensor-test.cpp
+++ test/tensor-test.cpp
@@ -4,47 +4,63 @@
 using namespace simit;
 
 TEST(Tensor, scalar) {
-  Scalar<FLOAT> scalar;
-  ASSERT_EQ(scalar.getOrder(), 0u);
+  Scalar<double> s(3.0);
+  ASSERT_EQ(s.getOrder(), 0u);
+  ASSERT_EQ(3.0, s);
+  s = 5.0;
+  ASSERT_EQ(5.0, s);
 }
 
 TEST(Tensor, vector) {
-  Vector<FLOAT> vector(3);
-  ASSERT_EQ(vector.getOrder(), 1u);
-  ASSERT_EQ(3u, vector.getDimension(0));
+  Vector<double> vec(3);
+  ASSERT_EQ(vec.getOrder(), 1u);
+  ASSERT_EQ(3u, vec.getDimension(0));
+  vec(0) = 3.0;
+  vec(1) = 2.0;
+  vec(2) = 7.0;
+  ASSERT_EQ(3.0, vec(0));
+  ASSERT_EQ(2.0, vec(1));
+  ASSERT_EQ(7.0, vec(2));
 }
 
 TEST(Tensor, matrix) {
-  Matrix<FLOAT> matrix(3, 2);
-  ASSERT_EQ(matrix.getOrder(), 2u);
-  ASSERT_EQ(3u, matrix.getDimension(0));
-  ASSERT_EQ(2u, matrix.getDimension(1));
+  Matrix<double> mat(3, 2);
+  ASSERT_EQ(mat.getOrder(), 2u);
+  ASSERT_EQ(3u, mat.getDimension(0));
+  ASSERT_EQ(2u, mat.getDimension(1));
+  mat(0,0) = 3.0;
+  mat(2,1) = 2.0;
+  mat(1,0) = 7.0;
+  ASSERT_EQ(3.0, mat(0,0));
+  ASSERT_EQ(2.0, mat(2,1));
+  ASSERT_EQ(7.0, mat(1,0));
 }
 
 TEST(Tensor, tensor3) {
-  Tensor<FLOAT,3> tensor(3, 2, 2);
+  Tensor<double,3> tensor(3, 2, 2);
   ASSERT_EQ(tensor.getOrder(), 3u);
   ASSERT_EQ(3u, tensor.getDimension(0));
   ASSERT_EQ(2u, tensor.getDimension(1));
   ASSERT_EQ(2u, tensor.getDimension(2));
+  tensor(0,0,1) = 3.0;
+  tensor(2,1,0) = 2.0;
+  tensor(1,0,1) = 7.0;
+  ASSERT_EQ(3.0, tensor(0,0,1));
+  ASSERT_EQ(2.0, tensor(2,1,0));
+  ASSERT_EQ(7.0, tensor(1,0,1));
 }
 
 TEST(Tensor, tensor4) {
-  Tensor<FLOAT,4> tensor(3, 2, 2, 2);
+  Tensor<double,4> tensor(3, 2, 2, 2);
   ASSERT_EQ(tensor.getOrder(), 4u);
   ASSERT_EQ(3u, tensor.getDimension(0));
   ASSERT_EQ(2u, tensor.getDimension(1));
   ASSERT_EQ(2u, tensor.getDimension(2));
   ASSERT_EQ(2u, tensor.getDimension(3));
-
-}
-
-TEST(Tensor, tensor5) {
-  Tensor<FLOAT,5> tensor(3, 2, 2, 2, 2);
-  ASSERT_EQ(tensor.getOrder(), 5u);
-  ASSERT_EQ(3u, tensor.getDimension(0));
-  ASSERT_EQ(2u, tensor.getDimension(1));
-  ASSERT_EQ(2u, tensor.getDimension(2));
-  ASSERT_EQ(2u, tensor.getDimension(3));
-  ASSERT_EQ(2u, tensor.getDimension(4));
+  tensor(0,0,1,0) = 3.0;
+  tensor(2,1,0,0) = 2.0;
+  tensor(1,0,1,1) = 7.0;
+  ASSERT_EQ(3.0, tensor(0,0,1,0));
+  ASSERT_EQ(2.0, tensor(2,1,0,0));
+  ASSERT_EQ(7.0, tensor(1,0,1,1));
 }
