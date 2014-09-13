4ddc8566c8ea90c5a8ce0221fa8316d75cc7bb1e
Fredrik Kjolstad <fred@csail.mit.edu>
2014-09-13 11:09:40 -0400

    Simplified

--- test/simit-test.cpp
+++ test/simit-test.cpp
@@ -4,23 +4,23 @@
 int main(int argc, char **argv) {
   // If there is just one argument and it is not a gtest option, then filter
   // the tests using that argument surrounded by wildcards.
-  std::string newArg;
-  if (argc == 2 && !(argv[1][0] == '-' && argv[1][1] == '-')) {
-    newArg = std::string("--gtest_filter=");
+  size_t lastArgLen = strlen(argv[argc-1]);
+  std::string filter;
+  if (argc > 1 &&
+      (lastArgLen == 1 ||
+       (lastArgLen >= 2 && std::string(argv[argc-1]).substr(0,2) != "--"))) {
+    filter = std::string(argv[1]);
 
     char *dotPtr = strchr(argv[1], '.');
     if (!dotPtr) {
-      newArg += "*" + std::string(argv[1]) + "*";
+      filter = "*" + filter + "*";
     }
-    else {
-      if (dotPtr[1] == '\0') {
-        newArg += std::string(argv[1]) + "*";
-      }
-      else {
-        newArg += std::string(argv[1]);
-      }
+    else if (dotPtr[1] == '\0') {
+      filter = filter + "*";
     }
-    argv[1] = (char*)newArg.c_str();
+
+    filter = std::string("--gtest_filter=") + filter;
+    argv[1] = (char*)filter.c_str();
   }
 
   ::testing::InitGoogleTest(&argc, argv);
