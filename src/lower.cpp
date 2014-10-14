9eb125b39d3ba216efece6ff318e27b4fcaa338e
Fredrik Kjolstad <fred@csail.mit.edu>
2014-10-14 02:36:52 -0400

    Adds lowering declarations

---
+++ src/lower.cpp
@@ -0,0 +1,12 @@
+#include "lower.h"
+
+#include "ir_mutator.h"
+
+namespace simit {
+namespace ir {
+
+Func lowerIndexExpressions(const Func &func) {
+  return Func();
+}
+
+}}
---
+++ src/lower.h
@@ -0,0 +1,18 @@
+#ifndef SIMIT_LOWER_H
+#define SIMIT_LOWER_H
+
+#include "ir.h"
+
+namespace simit {
+namespace ir {
+
+Func lowerIndexExpressions(Func func);
+//Function lowerMaps(Func func);
+
+//Function lowerWrites(Func func);
+//Function lowerLiterals(Func func);
+//Function lowerIntrinsics(Func func);
+
+}} // namespace simit::ir
+
+#endif
