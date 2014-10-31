f50d416622e63cfe48e4b9914cf9f1e9ecdc5951
Shoaib Kamil <shoaibkamil@gmail.com>
2014-10-31 07:40:22 -0400

    Add runtime.h

---
+++ src/runtime.h
@@ -0,0 +1,27 @@
+#ifndef SIMIT_RUNTIME_H
+#define SIMIT_RUNTIME_H
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+extern "C" {
+double cMatSolve(double* bufferA, double* bufferX,
+  double* bufferC, int rows, int columns) {
+  auto Amat = new Eigen::Map<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,
+                                      Eigen::RowMajor>>(bufferA, rows, columns);
+
+  auto xvec = new Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1>>(bufferX,
+    rows);
+
+  auto cvec = new Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1>>(bufferC,
+    rows);
+
+  // TODO: this may be overkill
+  // we can probably get away with LLT or LDLT instead
+  *cvec = Amat->colPivHouseholderQr().solve(*xvec);
+  
+  return 1.0;
+}
+}
+
+#endif
