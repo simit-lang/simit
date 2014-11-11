c3f294c6552157e5b6023e831f9be2cc6ea4d393
Fredrik Kjolstad <fred@csail.mit.edu>
2014-11-11 01:08:16 -0500

    Revert "Dump JITed assembly to stdout"
    This reverts commit eafa89809fa2aff25daad61e130dda8570449ab3.
    
    The reason is that this commit requires llvm 3.4 which currently causes
    random crashes. To be reintroduced later.

--- src/llvm_function.cpp
+++ src/llvm_function.cpp
@@ -16,7 +16,6 @@
 
 #include "llvm_codegen.h"
 #include "graph.h"
-#include "dis.h"
 
 using namespace std;
 
@@ -52,10 +51,6 @@ LLVMFunction::LLVMFunction(ir::Func simitFunc, llvm::Function *llvmFunc,
 
   fpm.doInitialization();
   fpm.run(*llvmFunc);
-  
-  // TODO: leaks
-  llvm::JITEventListener* disassembler = new DisassemblerJITEventListener();
-  executionEngine->RegisterJITEventListener(disassembler);
 }
 
 LLVMFunction::~LLVMFunction() {
