df1bc8e27db3464cab6f75899de4be5adb1ce375
Fredrik Kjolstad <fred@csail.mit.edu>
2014-11-13 06:57:07 -0500

    FEM2d test: Works through f vec construction

--- test/program-test.cpp
+++ test/program-test.cpp
@@ -1216,3 +1216,188 @@ end
   ASSERT_DOUBLE_EQ(1.1, (double)b.get(v2));
   ASSERT_DOUBLE_EQ(0.1, (double)b.get(v3));
 }
+
+
+#define VX(ii,jj) ( (jj)*(sx+1) + (ii))
+
+TEST(Program, fem) {
+  Program program;
+  std::string programText = R"(
+element Trig
+  u : float;
+  l : float;
+  m : float;
+end
+
+element Vert
+  X : tensor[2](float);
+  x : tensor[2](float);
+  v : tensor[2](float);
+
+  print : tensor[2](float);
+end
+
+extern verts : set{Vert};
+extern trigs : set{Trig}(verts, verts);
+
+func distribute_masses(t : Trig, v : (Vert*3)) ->
+    (M : tensor[verts](tensor[2](float)))
+  eye1 = [1.0, 1.0];
+  M(v(0)) = (1.0/3.0)*t.m*eye1;
+  M(v(1)) = (1.0/3.0)*t.m*eye1;
+  M(v(2)) = (1.0/3.0)*t.m*eye1;
+end
+
+
+func trace2(m: tensor[2,2](float)) -> (v : float)
+  v = m(0,0) + m(1,1);
+end
+
+func compute_force(t : Trig, v : (Vert*3)) ->
+    (f : tensor[verts](tensor[2](float)))
+  mu = 10.0;
+  lambda = 100.0;
+
+  eye2 = [1.0, 0.0; 0.0, 1.0];
+  e0 = v(1).x - v(0).x;
+  e1 = v(2).x - v(0).x;
+  e2 = v(2).x - v(1).x;
+  E0 = v(1).X - v(0).X;
+  E1 = v(2).X - v(0).X;
+  F = [0.0, 0.0; 0.0, 0.0];
+  F(0,0) = e0(0);
+  F(0,1) = e0(1);
+  F(1,0) = e1(0);
+  F(1,1) = e1(1);
+
+  m = [0.0, 0.0; 0.0, 0.0];
+  m(0,0) = E0(0);
+  m(0,1) = E0(1);
+  m(1,0) = E1(0);
+  m(1,1) = E1(1);
+
+  det2m = m(0,0) * m(1,1) - m(0,1) * m(1,0);
+
+  inv2m = [0.0, 0.0; 0.0, 0.0];
+
+  d = 1.0/det2m;
+  inv2m(0,0) =  m(1,1);
+  inv2m(0,1) = -m(0,1);
+  inv2m(1,0) = -m(1,0);
+  inv2m(1,1) =  m(0,0);
+  inv2m = d * inv2m;
+
+  F = F * inv2m;
+  FtF = F'*F;
+  E = 0.5 * (FtF-eye2);
+  trace2E = E(0,0) + E(1,1);
+  P = mu * E + lambda * trace2E * eye2;
+  J = F(0,0) * F(1,1) - F(0,1) * F(1,0);
+  stress = (P*F')./J;
+
+  n0 = [0.0, 0.0];
+  n0(0) = e0(1);
+  n0(1) = -e0(0);
+  fe0 = stress*n0;
+
+  n1 = [0.0, 0.0];
+  n1(0) = e2(1);
+  n1(1) = -e2(0);
+  fe1 = stress*n1;
+
+  n2 = [0.0, 0.0];
+  n2(0) = -e1(1);
+  n2(1) = e1(0);
+  fe2 = stress*n2;
+
+  f(v(1)) = -0.5*fe0 - 0.5*fe1;
+  f(v(0)) = -0.5*fe0 - 0.5*fe2;
+  f(v(2)) = -0.5*fe1 - 0.5*fe2;
+end
+
+proc main
+  h = 0.0002;
+  verts.x  = verts.x + h*verts.v;
+
+%  M = map distribute_masses to trigs with verts reduce +;
+  f = map compute_force to trigs with verts reduce +;
+
+  verts.print = --f;
+
+%  v = 0.99*v + h*f./M;
+%  verts.v = v;
+end
+  )";
+
+  simit::Set<> verts;
+  simit::Set<3> trigs(verts,verts,verts);
+  simit::FieldRef<double,2> X = verts.addField<double,2>("X");
+  simit::FieldRef<double,2> x = verts.addField<double,2>("x");
+  simit::FieldRef<double,2> v = verts.addField<double,2>("v");
+  simit::FieldRef<double,2> print = verts.addField<double,2>("print");
+
+  simit::FieldRef<double> u = trigs.addField<double>("u");
+  simit::FieldRef<double> l = trigs.addField<double>("l");
+  simit::FieldRef<double> m = trigs.addField<double>("m");
+
+  double uval = 1000, lval = 10000, mval = 10;
+  int sx = 1;
+  int sy = 1;
+
+  //create nodes
+  vector<simit::ElementRef> vertRefs;
+  for(int jj =0; jj<sy+1; jj++){
+    for(int ii=0; ii<sx+1; ii++){
+      vertRefs.push_back(verts.addElement());
+      simit::ElementRef p = vertRefs.back();
+      X.set(p, {(double)ii, (double)jj});
+      x.set(p, {1.1*(double)ii, 1.1*(double)jj});
+      v.set(p, {0.0, 0.0} );
+    }
+  }
+
+  for(int jj =0; jj<sy; jj++){
+    for(int ii=0; ii<sx; ii++){
+      simit::ElementRef t = trigs.addElement(vertRefs[VX(ii,   jj)],
+                                               vertRefs[VX(ii+1, jj)],
+                                               vertRefs[VX(ii  , jj+1)]);
+      u.set(t, uval);
+      l.set(t, lval);
+      m.set(t, mval);
+      t = trigs.addElement(vertRefs[VX(ii+1, jj+1)],
+                             vertRefs[VX(ii  , jj+1)],
+                             vertRefs[VX(ii+1, jj  )]);
+      u.set(t, uval);
+      l.set(t, lval);
+      m.set(t, mval);
+      
+    }
+  }
+
+  std::cout << "Initial position" << std::endl;
+  for (auto &v : verts) {
+    std::cout << x.get(v) << std::endl;
+  }
+  std::cout << std::endl;
+
+  // Compile program and bind arguments
+  int errorCode = program.loadString(programText);
+  if (errorCode) FAIL() << program.getDiagnostics().getMessage();
+
+  std::unique_ptr<Function> func = program.compile("main");
+  if (!func) FAIL() << program.getDiagnostics().getMessage();
+
+  func->bind("verts", &verts);
+  func->bind("trigs", &trigs);
+  func->runSafe();
+
+  std::cout << "Print:" << std::endl;
+  for (auto &v : verts) {
+    std::cout << print.get(v) << std::endl;
+  }
+  std::cout << std::endl;
+  std::cout << "Final position" << std::endl;
+  for (auto &v : verts) {
+    std::cout << x.get(v) << std::endl;
+  }
+}
