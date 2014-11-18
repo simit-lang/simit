b839fab838b395ba8ba1cf1bcb43bf05e6cd146b
Fredrik Kjolstad <fred@csail.mit.edu>
2014-11-18 09:35:00 -0500

    Visualizer make fixes

--- src/visualizer/visualizer.h
+++ src/visualizer/visualizer.h
@@ -19,6 +19,7 @@ void drawEdges(Set<2>& edges, FieldRef<double,3> coordField,
                float r, float g, float b, float a);
 void drawFaces(Set<3>& faces, FieldRef<double,3> coordField,
                float r, float g, float b, float a);
+
 // Blocking functions with callbacks for animation
 void drawPointsBlocking(const Set<>& points, FieldRef<double,3> coordField,
                         float r, float g, float b, float a,
@@ -29,6 +30,7 @@ void drawEdgesBlocking(Set<2>& edges, FieldRef<double,3> coordField,
 void drawFacesBlocking(Set<3>& faces, FieldRef<double,3> coordField,
                        float r, float g, float b, float a,
                        std::function<void()> animate);
+
 // Blocking function without callbacks for animation
 void drawPointsBlocking(const Set<>& points, FieldRef<double,3> coordField,
                         float r, float g, float b, float a);
