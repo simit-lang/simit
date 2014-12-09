361a42fc3cac1cbc108c0892703f8b44595c62fc
Fredrik Kjolstad <fred@csail.mit.edu>
2014-12-09 15:15:03 -0500

    Adds some checks to the map lowering

--- src/lower_assemblies.cpp
+++ src/lower_assemblies.cpp
@@ -1,26 +1,61 @@
 #include "lower.h"
 
 #include "ir_rewriter.h"
+#include "tensor_storage.h"
 
 using namespace std;
 
 namespace simit {
 namespace ir {
 
+inline bool hasStorage(std::vector<Var> vars, const TensorStorages &storages) {
+  for (auto &var : vars) {
+    if (storages.find(var) == storages.end()) return false;
+  }
+  return true;
+}
+
+inline bool hasSameStorage(std::vector<Var> vars,
+                           const TensorStorages &storages) {
+  if (vars.size() == 0) return true;
+  TensorStorage::Kind storage = storages.at(vars[0]).getKind();
+  for (auto &var : vars) {
+    if (storages.at(var).getKind() != storage) return false;
+  }
+  return true;
+}
+
 class LowerAssemblies : public IRRewriter {
+public:
+  LowerAssemblies(const TensorStorages &storages)
+      : storages(storages) {}
+
 private:
+  TensorStorages storages;
+
   void visit(const Map *op) {
     // \todo We should only drop the map statements if it's bound Vars have
     // no uses (extend/invert UseDef to get DefUse info).
+    stmt = op;
+
+    iassert(hasStorage(op->vars, storages))
+        << "Every assembled tensor should have a storage descriptor";
+    tassert(hasSameStorage(op->vars, storages))
+        << "All assembled tensors in the same Map must have the same storage.";
+
+    if (op->vars.size() == 0
+        || storages.at(op->vars[0]).getKind() != TensorStorage::SystemNone) {
+      Stmt loweredMap = Pass::make();
+      stmt = Block::make(op, loweredMap);
+    }
+    else {
+      stmt = op;
+    }
   }
 };
 
-Stmt lowerAssemblies(Stmt stmt) {
-  return LowerAssemblies().rewrite(stmt);
-}
-
-Func lowerAssemblies(Func func) {
-  Stmt body = lowerAssemblies(func.getBody());
+Func lowerAssemblies(Func func, const TensorStorages &tensorStorages) {
+  Stmt body = LowerAssemblies(tensorStorages).rewrite(func.getBody());
   return Func(func, body);
 }
 
