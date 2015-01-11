02106e702966edbf1266e387a0f18c3151750ede
Fredrik Kjolstad <fred@csail.mit.edu>
2015-01-11 16:40:24 -0500

    Adds CG test

--- test/system-misc-tests.cpp
+++ test/system-misc-tests.cpp
@@ -316,3 +316,39 @@ TEST(System, misc_map_edgeset_no_endpoints_results) {
   ASSERT_EQ(2.0, (double)a.get(s0));
   ASSERT_EQ(4.0, (double)a.get(s1));
 }
+
+TEST(System, cg) {
+  Set<> points;
+  FieldRef<double>  b = points.addField<double>("b");
+  FieldRef<double>  c = points.addField<double>("c");
+  FieldRef<int>    id = points.addField<int>("id");
+
+  ElementRef p0 = points.add();
+  ElementRef p1 = points.add();
+  ElementRef p2 = points.add();
+
+  b.set(p0, 1.0);
+  b.set(p1, 2.0);
+  b.set(p2, 3.0);
+
+  Set<2> springs(points,points);
+  FieldRef<double> a = springs.addField<double>("a");
+
+  ElementRef s0 = springs.add(p0,p1);
+  ElementRef s1 = springs.add(p1,p2);
+
+  a.set(s0, 4.0);
+  a.set(s1, 5.0);
+
+  std::unique_ptr<Function> f = getFunction(TEST_FILE_NAME, "main");
+  if (!f) FAIL();
+
+  f->bind("points", &points);
+  f->bind("springs", &springs);
+
+  f->runSafe();
+
+  ASSERT_EQ(0.9588, (double)c.get(p0));
+  ASSERT_EQ(1.9879, (double)c.get(p1));
+  ASSERT_EQ(3.0533, (double)c.get(p2));
+}
