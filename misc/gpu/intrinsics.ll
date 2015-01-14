2331fcb86d47f931495e028394bed6970ed0605d
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2015-01-14 01:46:30 -0500

    Patches for CUDA 7 NVVM
    It demands target triples (which contain nvptx[64]-*)

--- misc/gpu/intrinsics.ll
+++ misc/gpu/intrinsics.ll
@@ -1,4 +1,6 @@
 ; Intentionally blank. We can include LL ASM GPU intrinsic implementations, as well.
+; NVVM 7.0 demands a target triple
+target triple = "nvptx64-nvidia-cuda"
 
 define float @__test_f(float %x) {
   %xx = fmul float %x, %x
--- src/gpu_backend/gpu_codegen.cpp
+++ src/gpu_backend/gpu_codegen.cpp
@@ -26,14 +26,19 @@ llvm::Module *createNVVMModule(std::string name) {
   llvm::Module *module = new llvm::Module(name, LLVM_CONTEXT);
 
   // Set appropriate data layout
-  if (sizeof(void*) == 8)
+  if (sizeof(void*) == 8) {
     module->setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                           "i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-"
                           "v64:64:64-v128:128:128-n16:32:64");
-  else
+    module->setTargetTriple("nvptx64-nvidia-cuda");
+  }
+  else {
+    not_supported_yet << "We haven't tested PTX32 at all, and several things are known to overlook this.";
     module->setDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                           "i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-"
                           "v64:64:64-v128:128:128-n16:32:64");
+    module->setTargetTriple("nvptx-nvidia-cuda");
+  }
   return module;
 }
 
