ca119dc8e7d0376228534ea61291a99b70faa4c0
Gurtej Kanwar <gurtej@mit.edu>
2015-01-17 17:23:23 -0500

    Moves RW analysis to general simit directory

--- src/gpu_backend/kernel_rw_analysis.cpp
+++ src/gpu_backend/kernel_rw_analysis.cpp
@@ -2,6 +2,7 @@
 #include "gpu_ir.h"
 
 #include "ir_rewriter.h"
+#include "rw_analysis.h"
 
 namespace simit {
 namespace ir {
@@ -33,90 +34,6 @@ private:
   std::set<Var> rootVars;
 };
 
-class ReadWriteAnalysis : public IRVisitor {
-public:
-  ReadWriteAnalysis(std::set<Var> rootVars) {
-    this->rootVars = rootVars;
-  }
-
-  std::set<Var> getReads() {
-    return reads;
-  }
-
-  std::set<Var> getWrites() {
-    return writes;
-  }
-
-  using IRVisitor::visit;
-
-  void visit(const VarDecl *op) {
-    IRVisitor::visit(op);
-  }
-
-  void visit(const VarExpr *op) {
-    maybeRead(op->var);
-    IRVisitor::visit(op);
-  }
-  void visit(const CallStmt *op) {
-    for (Var result : op->results) {
-      maybeWrite(result);
-    }
-    IRVisitor::visit(op);
-  }
-  void visit(const ForRange *op) {
-    maybeRead(op->var);
-    IRVisitor::visit(op);
-  }
-  void visit(const For *op) {
-    maybeRead(op->domain.var);
-    IRVisitor::visit(op);
-  }
-
-  // Writes
-  void visit(const AssignStmt *op) {
-    if (op->cop.kind != CompoundOperator::None) {
-      maybeRead(op->var);
-    }
-    maybeWrite(op->var);
-    IRVisitor::visit(op);
-  }
-  void visit(const Store *op) {
-    // TODO(gkanwar): Fix this nasty casework
-    if (isa<VarExpr>(op->buffer)) {
-      maybeWrite(to<VarExpr>(op->buffer)->var);
-    }
-    else if (isa<FieldRead>(op->buffer)) {
-      const FieldRead* fieldRead = to<FieldRead>(op->buffer);
-      iassert(isa<VarExpr>(fieldRead->elementOrSet));
-      maybeWrite(to<VarExpr>(fieldRead->elementOrSet)->var);
-    }
-    else {
-      not_supported_yet;
-    }
-    IRVisitor::visit(op);
-  }
-  void visit(const FieldWrite *op) {
-    iassert(isa<VarExpr>(op->elementOrSet));
-    maybeWrite(to<VarExpr>(op->elementOrSet)->var);
-    IRVisitor::visit(op);
-  }
-
-private:
-  void maybeRead(Var var) {
-    if (rootVars.find(var) != rootVars.end()) {
-      reads.insert(var);
-    }
-  }
-  void maybeWrite(Var var) {
-    if (rootVars.find(var) != rootVars.end()) {
-      writes.insert(var);
-    }
-  }
-
-  std::set<Var> rootVars;
-  std::set<Var> reads;
-  std::set<Var> writes;
-};
 
 class KernelRWAnalysisRewriter : public IRRewriter {
 public:
---
+++ src/rw_analysis.h
@@ -0,0 +1,96 @@
+#ifndef SIMIT_RW_ANALYSIS
+#define SIMIT_RW_ANALYSIS
+
+#include "ir_visitor.h"
+
+namespace simit {
+namespace ir {
+
+class ReadWriteAnalysis : public IRVisitor {
+public:
+  ReadWriteAnalysis(std::set<Var> vars) {
+    this->vars = vars;
+  }
+
+  std::set<Var> getReads() {
+    return reads;
+  }
+
+  std::set<Var> getWrites() {
+    return writes;
+  }
+
+  using IRVisitor::visit;
+
+  void visit(const VarDecl *op) {
+    IRVisitor::visit(op);
+  }
+
+  void visit(const VarExpr *op) {
+    maybeRead(op->var);
+    IRVisitor::visit(op);
+  }
+  void visit(const CallStmt *op) {
+    for (Var result : op->results) {
+      maybeWrite(result);
+    }
+    IRVisitor::visit(op);
+  }
+  void visit(const ForRange *op) {
+    maybeRead(op->var);
+    IRVisitor::visit(op);
+  }
+  void visit(const For *op) {
+    maybeRead(op->domain.var);
+    IRVisitor::visit(op);
+  }
+
+  // Writes
+  void visit(const AssignStmt *op) {
+    if (op->cop.kind != CompoundOperator::None) {
+      maybeRead(op->var);
+    }
+    maybeWrite(op->var);
+    IRVisitor::visit(op);
+  }
+  void visit(const Store *op) {
+    // TODO(gkanwar): Fix this nasty casework
+    if (isa<VarExpr>(op->buffer)) {
+      maybeWrite(to<VarExpr>(op->buffer)->var);
+    }
+    else if (isa<FieldRead>(op->buffer)) {
+      const FieldRead* fieldRead = to<FieldRead>(op->buffer);
+      iassert(isa<VarExpr>(fieldRead->elementOrSet));
+      maybeWrite(to<VarExpr>(fieldRead->elementOrSet)->var);
+    }
+    else {
+      not_supported_yet;
+    }
+    IRVisitor::visit(op);
+  }
+  void visit(const FieldWrite *op) {
+    iassert(isa<VarExpr>(op->elementOrSet));
+    maybeWrite(to<VarExpr>(op->elementOrSet)->var);
+    IRVisitor::visit(op);
+  }
+
+private:
+  void maybeRead(Var var) {
+    if (vars.find(var) != vars.end()) {
+      reads.insert(var);
+    }
+  }
+  void maybeWrite(Var var) {
+    if (vars.find(var) != vars.end()) {
+      writes.insert(var);
+    }
+  }
+
+  std::set<Var> vars;
+  std::set<Var> reads;
+  std::set<Var> writes;
+};
+
+}}  // namespace simit::ir
+
+#endif
