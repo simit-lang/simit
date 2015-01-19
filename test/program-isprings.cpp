caa75684f47ab729621c03ad8069a914ff7527fd
Fredrik Kjolstad <fred@csail.mit.edu>
2015-01-19 00:43:54 -0500

    Updates isprings to eval version

--- test/program-isprings.cpp
+++ test/program-isprings.cpp
@@ -90,19 +90,24 @@ TEST(Program, isprings_simple) {
 }
 
 TEST(Program, isprings) {
+  double l0_       = 1.0;
+  double stiffness = 100.00;
+  double density   = 1000.0;
+  double radius = 0.01;
+  double PI_ = 3.14159265358979;
+  double zfloor = 0.4;
+
   // Points
   Set<> points;
   simit::FieldRef<simit_float,3> x = points.addField<simit_float,3>("x");
   simit::FieldRef<simit_float,3> v = points.addField<simit_float,3>("v");
+  simit::FieldRef<simit_float> m = points.addField<simit_float>("m");
   simit::FieldRef<bool> fixed = points.addField<bool>("fixed");
-
-  simit::FieldRef<simit_float,3> ones = points.addField<simit_float,3>("ones");
 
   // Springs
   Set<2> springs(points,points);
   simit::FieldRef<simit_float> k = springs.addField<simit_float>("k");
   simit::FieldRef<simit_float> l0 = springs.addField<simit_float>("l0");
-  simit::FieldRef<simit_float> m = springs.addField<simit_float>("m");
 
   // Build a 3-chain
   ElementRef p0 = points.add();
@@ -117,26 +122,19 @@ TEST(Program, isprings) {
   v(p1) = {0.0, 0.0, 0.0};
   v(p2) = {0.0, 0.0, 0.0};
 
-  ones(p0) = {1.0, 1.0, 1.0};
-  ones(p1) = {1.0, 1.0, 1.0};
-  ones(p2) = {1.0, 1.0, 1.0};
+  m(p0) = PI_*radius*radius*l0_*density;
+  m(p1) = PI_*radius*radius*l0_*density;
+  m(p2) = PI_*radius*radius*l0_*density;
 
   ElementRef s0 = springs.add(p0,p1);
   ElementRef s1 = springs.add(p1,p2);
 
-  double l0_       = 1.0;
-  double stiffness = 100.00;
-  double density   = 1000.0;
-  double radius = 0.01;
-  double PI_ = 3.14159265358979;
-  double zfloor = 0.4;
-
   l0(s0) = l0_;
   l0(s1) = l0_;
+
   k(s0) = stiffness;
   k(s1) = stiffness;
-  m(s0) = PI_*radius*radius*l0_*density;
-  m(s1) = PI_*radius*radius*l0_*density;
+
 
   for (auto &p : points) {
     if (x(p)(2) < zfloor) {
@@ -152,49 +150,27 @@ TEST(Program, isprings) {
   if (!f) FAIL();
   f->bind("points", &points);
   f->bind("springs", &springs);
-  for (size_t i=0; i < 9; ++i) {
+  for (size_t i=0; i < 10; ++i) {
     f->runSafe();
   }
 
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p1)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p1)(1));
-  ASSERT_SIMIT_FLOAT_EQ(-0.527683955297994, v(p1)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p2)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p2)(1));
-  ASSERT_SIMIT_FLOAT_EQ(-0.72137873080947,  v(p2)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p0)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p0)(1));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p0)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p1)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p1)(1));
+  ASSERT_SIMIT_FLOAT_EQ(-0.54482994491692449, v(p1)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p2)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  v(p2)(1));
+  ASSERT_SIMIT_FLOAT_EQ(-0.8480293605755741,  v(p2)(2));
 
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p0)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p0)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p0)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p1)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p1)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.967523428995587,  x(p1)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p2)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                x(p2)(1));
-  ASSERT_SIMIT_FLOAT_EQ(1.9602574003985656, x(p2)(2));
-
-  f->runSafe();
-
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p0)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p1)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p1)(1));
-  ASSERT_SIMIT_FLOAT_EQ(-0.533862882516396, v(p1)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p2)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,                v(p2)(1));
-  ASSERT_SIMIT_FLOAT_EQ(-0.754380624415897, v(p2)(2));
-
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p0)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p0)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p0)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p1)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p1)(1));
-  ASSERT_SIMIT_FLOAT_EQ(0.962184800170423, x(p1)(2));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p2)(0));
-  ASSERT_SIMIT_FLOAT_EQ(0.0,               x(p2)(1));
-  ASSERT_SIMIT_FLOAT_EQ(1.952713594154406, x(p2)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p0)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p0)(1));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p0)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p1)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p1)(1));
+  ASSERT_SIMIT_FLOAT_EQ(0.96180188527075183,  x(p1)(2));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p2)(0));
+  ASSERT_SIMIT_FLOAT_EQ(0.0,                  x(p2)(1));
+  ASSERT_SIMIT_FLOAT_EQ(1.9500008677096612,   x(p2)(2));
 }
