710629205d0e7bd15a57a92639ffea7df7055d58
Gurtej Kanwar <gurtej@mit.edu>
2015-01-19 04:27:59 -0500

    Moves var replace rewriter into a top-level file

--- src/gpu_backend/fuse_kernels.cpp
+++ src/gpu_backend/fuse_kernels.cpp
@@ -6,6 +6,7 @@
 #include "rw_analysis.h"
 
 #include "kernel_rw_analysis.h"
+#include "var_replace_rewriter.h"
 
 namespace simit {
 namespace ir {
@@ -93,91 +94,6 @@ private:
 
 Func canonicalizeBlocks(Func func, bool forward) {
   return CanonicalizeBlocksRewriter(forward).rewrite(func);
-}
-
-class VarReplaceRewriter : public IRRewriter {
-public:
-  VarReplaceRewriter(Var init, Var final) : init(init), final(final) {}
-
-  using IRRewriter::visit;
-
-  void visit(const VarExpr *op) {
-    if (op->var == init) {
-      expr = VarExpr::make(final);
-    }
-    else {
-      IRRewriter::visit(op);
-    }
-  }
-
-  void visit(const VarDecl *op) {
-    if (op->var == init) {
-      stmt = VarDecl::make(final);
-    }
-    else {
-      IRRewriter::visit(op);
-    }
-  }
-
-  void visit(const AssignStmt *op) {
-    Expr value = rewrite(op->value);
-    if (op->var == init) {
-      stmt = AssignStmt::make(final, value, op->cop);
-    }
-    else {
-      IRRewriter::visit(op);
-    }
-  }
-
-  void visit(const CallStmt *op) {
-    std::vector<Var> finalResults;
-    for (Var res : op->results) {
-      if (res == init)
-        finalResults.push_back(final);
-      else
-        finalResults.push_back(res);
-    }
-    std::vector<Expr> actuals(op->actuals.size());
-    for (size_t i=0; i < op->actuals.size(); ++i) {
-      actuals[i] = rewrite(op->actuals[i]);
-    }
-    stmt = CallStmt::make(finalResults, op->callee, actuals);
-  }
-
-  void visit(const ForRange *op) {
-    Expr start = rewrite(op->start);
-    Expr end = rewrite(op->end);
-    Stmt body = rewrite(op->body);
-    if (op->var == init) {
-      stmt = ForRange::make(final, start, end, body);
-    }
-    else {
-      IRRewriter::visit(op);
-    }
-  }
-
-  void visit(const For *op) {
-    Stmt body = rewrite(op->body);
-    if (op->domain.var == init) {
-
-      ForDomain domain = ForDomain(op->domain.set, final,
-                                   op->domain.kind, op->domain.indexSet);
-      stmt = For::make(op->var, domain, body);
-    }
-    else if (op->var == init) {
-      stmt = For::make(final, op->domain, body);
-    }
-    else {
-      IRRewriter::visit(op);
-    }
-  }
-
-private:
-  Var init, final;
-};
-
-Stmt replaceVar(Stmt stmt, Var init, Var final) {
-  return VarReplaceRewriter(init, final).rewrite(stmt);
 }
 
 class KernelSwapRewriter : public IRRewriter {
