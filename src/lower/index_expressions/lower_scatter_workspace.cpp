ae7ab7078d800e5051dcb30cd1cb8da6d5bf256e
Fredrik Kjolstad <fred@csail.mit.edu>
2015-08-12 10:06:49 -0400

    Removes a bunch of deprecated code

--- src/lower/index_expressions/lower_scatter_workspace.cpp
+++ src/lower/index_expressions/lower_scatter_workspace.cpp
@@ -20,9 +20,7 @@ namespace ir {
 
 typedef vector<IndexVar> IndexTuple;
 typedef map<IndexTuple, vector<const IndexedTensor *>> IndexTupleUses;
-typedef map<IndexVar, vector<const IndexedTensor *>> IndexUses;
 typedef map<IndexVar, vector<IndexVar>> IndexVarGraph;
-typedef map<IndexVar, pair<Var,vector<TensorIndexVar>>> InductionVars;
 
 ostream &operator<<(ostream &os, const IndexVarGraph &ivGraph) {
   os << "Index Variable Graph:"  << endl;
@@ -35,38 +33,12 @@ ostream &operator<<(ostream &os, const IndexVarGraph &ivGraph) {
   return os;
 }
 
-ostream &operator<<(ostream &os, const IndexUses &indexUses) {
-  os << "Index Variable Uses:" << std::endl;
-  for (auto &itu : indexUses) {
-    for (auto &it : itu.second) {
-      os << itu.first << " -> " << *it << endl;
-    }
-  }
-  return os;
-}
-
-ostream &operator<<(ostream &os, const IndexTupleUses &indexTupleUses) {
-  os << "Index Variable Tuple Uses:" << endl;
-  for (auto &itu : indexTupleUses) {
-    for (auto &it : itu.second) {
-      os << "(" << util::join(itu.first, ",") << ")" << " -> " << *it << endl;
-    }
-  }
-  return os;
-}
-
-ostream &operator<<(ostream &os, const InductionVars &inductionVars) {
-  os << "Loop induction variables:" << endl;
-  for (auto &inductionVar : inductionVars) {
-    os << inductionVar.second.first;
-    if (inductionVar.second.second.size() > 0) {
-      os << ": zip(" << util::join(inductionVar.second.second) << ")";
-    }
-    os << endl;
-  }
-  return os;
-}
-
+/// Build a map from index variable tuples to the IndexTensors they access:
+/// - B+C   (i,j) -> B(i,j), C(i,j)
+/// - B+C'  (i,j) -> B(i,j)
+///         (j,i) -> C(j,i)
+/// - B*C:  (i,k) -> B(i,k)
+///         (k,j) -> C(k,j)
 static IndexTupleUses getIndexTupleUses(const IndexExpr *indexExpr) {
   struct GetIndexTupleUsesVisitor : public IRVisitor {
     IndexTupleUses indexTupleUses;
@@ -79,7 +51,18 @@ static IndexTupleUses getIndexTupleUses(const IndexExpr *indexExpr) {
   return visitor.indexTupleUses;
 }
 
-static IndexVarGraph createIndexVarGraph(IndexTupleUses indexTupleUses) {
+/// Build a map from index variables to index variables they can reach through
+/// a usage. This map encodes a directed index variable graph where vertices
+/// are index variables, and where there exist an edge (i,j) if i and j are
+/// ever used together to index a tensor that has an index from i to j. For now
+/// we will assume we always have available all indices, but we may later want
+/// to optimize for memory by computing a minimum set of indices we need.
+/// - B+C: i -> j and j -> i
+/// - B*C: i -> k and k -> i
+///        k -> j and j -> k
+static IndexVarGraph createIndexVarGraph(const IndexExpr *indexExpression) {
+  IndexTupleUses indexTupleUses = getIndexTupleUses(indexExpression);
+
   IndexVarGraph indexVarGraph;
   for (auto &itu : indexTupleUses) {
     IndexTuple it = itu.first;
@@ -106,8 +89,11 @@ static void createLoopNest(const IndexVarGraph &ivGraph,
   }
 }
 
-static vector<IndexVariableLoop> createLoopNest(const IndexVarGraph &ivGraph,
-                                   const vector<IndexVar> &sources){
+/// Order the index variables into one loop per index variable, by traversing
+/// the index variable graph
+static
+vector<IndexVariableLoop> createLoopNest(const IndexVarGraph &ivGraph,
+                                         const vector<IndexVar> &sources){
   vector<IndexVariableLoop> loops;
   set<IndexVar> visited;
   for (auto &source : sources) {
@@ -121,53 +107,9 @@ static vector<IndexVariableLoop> createLoopNest(const IndexVarGraph &ivGraph,
   return loops;
 }
 
-/// Build a map from index variables to the IndexTensors they access.
-/// - B+C  i -> B(i,j), C(i,j)
-///        j -> B(i,j), C(i,j)
-static IndexUses getIndexUses(IndexTupleUses indexTupleUses) {
-  IndexUses indexUses;
-  for (auto &itu : indexTupleUses) {
-    for (auto &indexVar : itu.first) {
-      for (auto &indexedTensor : itu.second) {
-        indexUses[indexVar].push_back(indexedTensor);
-      }
-    }
-  }
-  return indexUses;
-}
-
-static
-InductionVars createInductionVariables(const vector<IndexVariableLoop> &loops,
-                                       const IndexTupleUses &indexTupleUses) {
-  IndexUses indexUses = getIndexUses(indexTupleUses);
-
-  InductionVars inductionVars;
-  for (auto &loop : loops) {
-    IndexVar indexVar = loop.getIndexVar();
-
-    Var inductionVar = loop.getInductionVar();
-    inductionVars[indexVar].first = inductionVar;
-
-    if (loop.isLinked()) {
-      IndexVar linkedIndexVar = loop.getLinkedLoop().getIndexVar();
-      Var linkedInductionVar  = loop.getLinkedLoop().getInductionVar();
-
-      vector<const IndexedTensor *> uses = indexUses.at(indexVar);
-      for (auto &indexedTensor : uses) {
-        iassert(isa<VarExpr>(indexedTensor->tensor))
-            << "at this point the index expressions should have been flattened";
-        Var tensor = to<VarExpr>(indexedTensor->tensor)->var;
-        vector<IndexVar> indexVars = indexedTensor->indexVars;
-
-        TensorIndexVar indexInductionVar(inductionVar, linkedInductionVar,
-                                         tensor,
-                                         util::locate(indexVars,linkedIndexVar),
-                                         util::locate(indexVars,indexVar));
-        inductionVars.at(indexVar).second.push_back(indexInductionVar);
-      }
-    }
-  }
-  return inductionVars;
+static vector<IndexVariableLoop> createLoopNest(const IndexExpr *indexExpr) {
+  IndexVarGraph indexVariableGraph = createIndexVarGraph(indexExpr);
+  return createLoopNest(indexVariableGraph, indexExpr->resultVars);
 }
 
 static Expr compareToNextIndexLocation(const TensorIndexVar &inductionVar) {
@@ -276,41 +218,7 @@ static string workspaceWriteString(const SubsetLoop &subsetLoop,
 }
 
 Stmt lowerScatterWorkspace(Expr target, const IndexExpr *indexExpression) {
-  // Build a map from index variable tuples to the IndexTensors they access:
-  // - B+C   (i,j) -> B(i,j), C(i,j)
-  // - B+C'  (i,j) -> B(i,j)
-  //         (j,i) -> C(j,i)
-  // - B*C:  (i,k) -> B(i,k)
-  //         (k,j) -> C(k,j)
-  IndexTupleUses indexTupleUses = getIndexTupleUses(indexExpression);
-  std::cout << indexTupleUses << std::endl;
-
-
-  // Build a map from index variables to index variables they can reach through
-  // a usage. This map encodes a directed index variable graph where vertices
-  // are index variables, and where there exist an edge (i,j) if i and j are
-  // ever used together to index a tensor that has an index from i to j. For now
-  // we will assume we always have available all indices, but we may later want
-  // to optimize for memory by computing a minimum set of indices we need.
-  // - B+C: i -> j and j -> i
-  // - B*C: i -> k and k -> i
-  //        k -> j and j -> k
-  IndexVarGraph indexVariableGraph = createIndexVarGraph(indexTupleUses);
-  std::cout << indexVariableGraph << std::endl;
-
-
-  // Order the index variables into one loop per index variable, by traversing
-  // the index variable graph
-  vector<IndexVariableLoop> loops = createLoopNest(indexVariableGraph,
-                                                   indexExpression->resultVars);
-
-
-  // Create Loop Inducation Variables and Coordinate Induction Variables:
-  // - B+C  i
-  //        j: zip(ijB in nbr(B), ijC in nbr(C))
-  InductionVars inductionVars = createInductionVariables(loops, indexTupleUses);
-//  std::cout << inductionVars << std::endl;
-
+  vector<IndexVariableLoop> loops = createLoopNest(indexExpression);
 
   // Emit loops
   Stmt loopNest;
