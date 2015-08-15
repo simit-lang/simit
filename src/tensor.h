caef718ed376c08f99d41522c8228ea922e4715b
Fredrik Kjolstad <fred@csail.mit.edu>
2015-08-15 11:22:56 -0400

    Adds DenseTensor class
    This class will replace Tensor and is typed on component type and
    dimensions.

--- src/tensor.h
+++ src/tensor.h
@@ -1,8 +1,12 @@
 #ifndef SIMIT_TENSOR_H
 #define SIMIT_TENSOR_H
 
+#include <iostream>
 #include <ostream>
-#include <memory>
+#include <iomanip>
+
+#include "util/compare.h"
+#include "variadic.h"
 
 namespace simit {
 
@@ -41,6 +45,170 @@ private:
   struct Content;
   Content *content;
 };
+
+template <typename ComponentType, int... Dimensions>
+class DenseTensor {
+public:
+  DenseTensor() : data(new ComponentType[getSize()]) {
+    std::fill(&data[0], &data[getSize()], ComponentType());
+  }
+
+  DenseTensor(const ComponentType &val) : data(new ComponentType(val)) {
+    static_assert(getSize() == 1, "Using scalar constructor with non-scalar");
+  }
+
+  DenseTensor(const std::initializer_list<ComponentType>& vals) : DenseTensor(){
+    std::copy(vals.begin(), vals.end(), data);
+  }
+
+  DenseTensor(const DenseTensor& other) : DenseTensor() {
+    std::copy(&other.data[0], &other.data[getSize()], data);
+  }
+
+  DenseTensor(DenseTensor&& other) noexcept : data(other.data) {
+    other.data = nullptr;
+  }
+
+  DenseTensor& operator=(const DenseTensor& other) {
+    if (&other == this) {
+      return *this;
+    }
+    DenseTensor tmp(other);
+    *this = std::move(tmp);
+    return *this;
+  }
+
+  DenseTensor& operator=(DenseTensor&& other) noexcept {
+    std::swap(data, other.data);
+    return *this;
+  }
+  
+  ~DenseTensor() {
+    delete[] data;
+  }
+
+  static size_t getSize() {return util::product<Dimensions...>::value;}
+
+  template <typename... Indices> inline
+  ComponentType& operator()(Indices... index) {
+    static_assert(sizeof...(index) == sizeof...(Dimensions),
+                  "Incorrect number of indices used to index tensor");
+    return data[util::computeOffset(util::seq<Dimensions...>(), index...)];
+  }
+
+  template <typename... Indices> inline
+  const ComponentType& operator()(Indices... index) const {
+    static_assert(sizeof...(index) == sizeof...(Dimensions),
+                  "Incorrect number of indices used to index tensor");
+    return data[util::computeOffset(util::seq<Dimensions...>(), index...)];
+  }
+
+  const ComponentType* getData() const {return data;}
+  ComponentType* getData() {return data;}
+
+private:
+  ComponentType *data;
+};
+
+template <int... Dimensions>
+bool operator==(const DenseTensor<float,Dimensions...>& l,
+                const DenseTensor<float,Dimensions...>& r) {
+  const float* ldata = l.getData();
+  const float* rdata = r.getData();
+  for (size_t i=0; i < l.getSize(); ++i) {
+    if (!util::almostEqual(ldata[i], rdata[i], 0.0001f)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <int... Dimensions>
+bool operator==(const DenseTensor<double,Dimensions...>& l,
+                const DenseTensor<double,Dimensions...>& r) {
+  const double* ldata = l.getData();
+  const double* rdata = r.getData();
+  for (size_t i=0; i < l.getSize(); ++i) {
+    if (!util::almostEqual(ldata[i], rdata[i], 0.0001)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <typename ComponentType, int... Dimensions>
+bool operator==(const DenseTensor<ComponentType,Dimensions...>& l,
+                const DenseTensor<ComponentType,Dimensions...>& r) {
+  const ComponentType* ldata = l.getData();
+  const ComponentType* rdata = r.getData();
+  for (size_t i=0; i < l.getSize(); ++i) {
+    if (ldata[i] != rdata[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/// Tensors with different types are different
+template <typename ComponentType1, int... Dimensions1,
+          typename ComponentType2, int... Dimensions2>
+bool operator==(const DenseTensor<ComponentType1,Dimensions1...>& l,
+                const DenseTensor<ComponentType2,Dimensions2...>& r) {
+  return false;
+}
+
+template <typename ComponentType, int... Dimensions>
+std::ostream&
+operator<<(std::ostream& os, const DenseTensor<ComponentType,Dimensions...>& t){
+  os << "tensor";
+  return os;
+}
+
+template <typename ComponentType, int rows, int cols>
+std::ostream&
+operator<<(std::ostream& os, const DenseTensor<ComponentType,rows,cols>& t) {
+  os << "matrix:\n";
+  std::ios::fmtflags osflags(os.flags());
+  os << std::setprecision(2) << std::fixed;
+  if (0 < rows) {
+    if (0 < cols) {
+      os << t(0,0);
+    }
+    for (int j=1; j<cols; ++j) {
+      os << " " << t(0,j);
+    }
+  }
+
+  for (int i=1; i<rows; ++i) {
+    os << "\n";
+    if (0 < cols) {
+      os << t(i,0);
+    }
+    for (int j=1; j<cols; ++j) {
+      os << " " << t(i,j);
+    }
+  }
+  os.flags(osflags);
+  return os;
+}
+
+template <typename ComponentType, int size>
+std::ostream&
+operator<<(std::ostream& os, const DenseTensor<ComponentType,size>& t) {
+  os << "[";
+  std::ios::fmtflags osflags(os.flags());
+  os << std::setprecision(2) << std::fixed;
+  if (0 < size) {
+    os << t(0);
+  }
+  for (int i=1; i<size; ++i) {
+    os << " " << t(i);
+  }
+  os.flags(osflags);
+  os << "]";
+  return os;
+}
+
 
 }
 #endif
---
+++ test/tensor-tests.cpp
@@ -0,0 +1,66 @@
+#include "simit-test.h"
+
+#include <vector>
+
+#include "tensor.h"
+
+using namespace std;
+using namespace simit;
+
+TEST(Tensor, Size) {
+  DenseTensor<double> scalar;
+  DenseTensor<double,1> vec1;
+  DenseTensor<double,1,1> mat11;
+  DenseTensor<double,1,1,1> ten111;
+  ASSERT_EQ(1u, scalar.getSize());
+  ASSERT_EQ(1u, vec1.getSize());
+  ASSERT_EQ(1u, mat11.getSize());
+  ASSERT_EQ(1u, ten111.getSize());
+
+  DenseTensor<double,2> vec2;
+  DenseTensor<double,3> vec3;
+  ASSERT_EQ(2u, vec2.getSize());
+  ASSERT_EQ(3u, vec3.getSize());
+
+  DenseTensor<double,2,2> mat22;
+  DenseTensor<double,2,3> mat23;
+  DenseTensor<double,3,2> mat32;
+  ASSERT_EQ(4u, mat22.getSize());
+  ASSERT_EQ(6u, mat23.getSize());
+  ASSERT_EQ(6u, mat32.getSize());
+
+  DenseTensor<double,2,2,2> ten222;
+  DenseTensor<double,2,2,3> ten223;
+  DenseTensor<double,2,4,2> ten242;
+  DenseTensor<double,5,2,2> ten522;
+  ASSERT_EQ(8u, ten222.getSize());
+  ASSERT_EQ(12u, ten223.getSize());
+  ASSERT_EQ(16u, ten242.getSize());
+  ASSERT_EQ(20u, ten522.getSize());
+}
+
+TEST(Tensor, index) {
+  DenseTensor<double,2,2> mat1 = {0.0, 1.0, 2.0, 0.0};
+  DenseTensor<double,2,2> mat2;
+  mat2(0,1) = 1.0;
+  mat2(1,0) = 2.0;
+  ASSERT_EQ(mat1, mat2);
+}
+
+TEST(Tensor, copy) {
+  DenseTensor<double,2,2> mat1;
+  mat1(0,1) = 1.0;
+
+  auto mat2(mat1);
+  mat2(1,0) = 2.0;
+
+  auto mat3 = mat2;
+  mat3(1,1) = 3.0;
+
+  DenseTensor<double,2,2> expected1 = {0.0, 1.0, 0.0, 0.0};
+  DenseTensor<double,2,2> expected2 = {0.0, 1.0, 2.0, 0.0};
+  DenseTensor<double,2,2> expected3 = {0.0, 1.0, 2.0, 3.0};
+  ASSERT_EQ(expected1, mat1);
+  ASSERT_EQ(expected2, mat2);
+  ASSERT_EQ(expected3, mat3);
+}
