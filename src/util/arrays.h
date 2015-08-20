a08ef2309ed3bcd08327b428ca3777de2819011f
Fredrik Kjolstad <fred@csail.mit.edu>
2015-08-19 21:30:27 -0400

    Adds printArray helper function

--- src/util/arrays.h
+++ src/util/arrays.h
@@ -6,13 +6,30 @@ namespace util {
 
 template <typename T>
 void zero(T* data, size_t size) {
-  T* tdata = static_cast<T*>(data);
-  std::fill(tdata, tdata+size, T());
+  std::fill(data, data+size, T());
 }
 template <typename T>
 void zero(void* data, size_t size) {
-  T* tdata = static_cast<T*>(data);
-  std::fill(tdata, tdata+size, T());
+  zero(static_cast<T*>(data), size);
+}
+
+
+template <typename T>
+static void printArray(std::ostream &os, const T* data, unsigned size,
+                       const std::string &separator=", ") {
+  if (size == 1) {
+    os << data[0];
+  }
+  else {
+    for (size_t i=1; i < size; ++i) {
+      os << ", " << data[i];
+    }
+  }
+}
+template <typename T>
+static void printArray(std::ostream &os, const void* data, unsigned size,
+                       const std::string &separator=", ") {
+  printArray(os, static_cast<T*>(data), size, separator);
 }
 
 }}
