2ad9d4177a7924a40391426d5262869bde2ba25c
Fredrik Kjolstad <fred@csail.mit.edu>
2015-09-05 13:24:03 -0400

    Adds checks to SPMV add test

--- test/index_expression-tests.cpp
+++ test/index_expression-tests.cpp
@@ -29,15 +29,8 @@ TEST(IndexExpression, add) {
   env.addExtern(to<VarExpr>(B)->var);
   env.addExtern(to<VarExpr>(C)->var);
 
-  Storage storage;
-
-
-  std::cout << env << std::endl << std::endl;
   Stmt loops = lowerScatterWorkspace(A, to<IndexExpr>(add), &env);
-  std::cout << loops << std::endl << std::endl;
-//  std::cout << env << std::endl << std::endl;
-
-  simit::Function function = getTestBackend()->compile(loops, env, storage);
+  simit::Function function = getTestBackend()->compile(loops, env);
 
   /// The size of V determines the dimensions of the matrices (we don't support
   /// sparse matrices with range dimensions yet).
@@ -48,28 +41,46 @@ TEST(IndexExpression, add) {
   // 1.0 2.0 0.0
   // 3.0 4.0 0.0
   // 0.0 0.0 0.0
-  int A_row_ptr[4] = {0, 2, 4, 4};
-  int A_col_ind[4] = {0, 1, 0, 1};
-  double A_vals[4] = {1.0, 2.0, 3.0, 4.0};
-  function.bind("A", A_row_ptr, A_col_ind, A_vals);
+  int B_rowPtr[4]  = {0, 2, 4, 4};
+  int B_colInd[4]  = {0, 1, 0, 1};
+  double B_vals[4] = {1.0, 2.0, 3.0, 4.0};
+  function.bind("B", B_rowPtr, B_colInd, B_vals);
 
   // 0.0 0.0 0.0
   // 0.0 0.1 0.2
   // 0.0 0.3 0.4
-  int B_row_ptr[4] = {0, 0, 2, 4};
-  int B_col_ind[4] = {1, 2, 1, 2};
-  double B_vals[4] = {0.1, 0.2, 0.3, 0.4};
-  function.bind("B", B_row_ptr, B_col_ind, B_vals);
+  int C_rowPtr[4]  = {0, 0, 2, 4};
+  int C_colInd[4]  = {1, 2, 1, 2};
+  double C_vals[4] = {0.1, 0.2, 0.3, 0.4};
+  function.bind("C", C_rowPtr, C_colInd, C_vals);
 
   // 1.0 2.0 0.0
   // 3.0 4.1 0.2
   // 0.0 0.3 0.4
-  int C_row_ptr[4] = {0, 2, 5, 7};
-  int C_col_ind[7] = {0, 1, 0, 1, 2, 1, 2};
-  double C_vals[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  function.bind("C", C_row_ptr, C_col_ind, C_vals);
+  int A_rowPtr[4] = {0, 2, 5, 7};
+  int A_colInd[7] = {0, 1, 0, 1, 2, 1, 2};
+  double A_vals[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  function.bind("A", A_rowPtr, A_colInd, A_vals);
 
-  std::cout << function << std::endl;
+  function.runSafe();
+
+  SIMIT_EXPECT_FLOAT_EQ(1.0, B_vals[0]);
+  SIMIT_EXPECT_FLOAT_EQ(2.0, B_vals[1]);
+  SIMIT_EXPECT_FLOAT_EQ(3.0, B_vals[2]);
+  SIMIT_EXPECT_FLOAT_EQ(4.0, B_vals[3]);
+
+  SIMIT_EXPECT_FLOAT_EQ(0.1, C_vals[0]);
+  SIMIT_EXPECT_FLOAT_EQ(0.2, C_vals[1]);
+  SIMIT_EXPECT_FLOAT_EQ(0.3, C_vals[2]);
+  SIMIT_EXPECT_FLOAT_EQ(0.4, C_vals[3]);
+
+  SIMIT_EXPECT_FLOAT_EQ(1.0, A_vals[0]);
+  SIMIT_EXPECT_FLOAT_EQ(2.0, A_vals[1]);
+  SIMIT_EXPECT_FLOAT_EQ(3.0, A_vals[2]);
+  SIMIT_EXPECT_FLOAT_EQ(4.1, A_vals[3]);
+  SIMIT_EXPECT_FLOAT_EQ(0.2, A_vals[4]);
+  SIMIT_EXPECT_FLOAT_EQ(0.3, A_vals[5]);
+  SIMIT_EXPECT_FLOAT_EQ(0.4, A_vals[6]);
 }
 
 TEST(IndexExpression, mul) {
