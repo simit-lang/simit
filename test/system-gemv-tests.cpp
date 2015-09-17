28da1047e4889ae253770214a90e9b7581591aa6
Fredrik Kjolstad <fred@csail.mit.edu>
2015-09-17 14:01:25 -0400

    Disabling gemm test
    And moves it to system-matrices-test.cpp

--- test/system-gemv-tests.cpp
+++ test/system-gemv-tests.cpp
@@ -8,55 +8,6 @@
 using namespace std;
 using namespace simit;
 
-TEST(System, gemm_simple) {
-  // Points
-  Set points;
-  FieldRef<simit_float> b = points.addField<simit_float>("b");
-  FieldRef<simit_float> c = points.addField<simit_float>("c");
-
-  ElementRef p0 = points.add();
-  ElementRef p1 = points.add();
-  ElementRef p2 = points.add();
-
-  b.set(p0, 1.0);
-  b.set(p1, 2.0);
-  b.set(p2, 3.0);
-
-  // Taint c
-  c.set(p0, 22.0);
-  c.set(p1, 35.0);
-  c.set(p2, 42.0);
-
-  // Springs
-  Set springs(points,points);
-  FieldRef<simit_float> a = springs.addField<simit_float>("a");
-
-  ElementRef s0 = springs.add(p0,p1);
-  ElementRef s1 = springs.add(p1,p2);
-
-  a.set(s0, 1.0);
-  a.set(s1, 2.0);
-
-  // Compile program and bind arguments
-  Function func = loadFunction(TEST_FILE_NAME, "main");
-  if (!func.defined()) FAIL();
-
-  func.bind("points", &points);
-  func.bind("springs", &springs);
-
-  func.runSafe();
-
-  // Check that inputs are preserved
-  //ASSERT_EQ(1.0, b.get(p0));
-  //ASSERT_EQ(2.0, b.get(p1));
-  //ASSERT_EQ(3.0, b.get(p2));
-
-  // Check that outputs are correct
-  //ASSERT_EQ(3.0, c.get(p0));
-  //ASSERT_EQ(13.0, c.get(p1));
-  //ASSERT_EQ(10.0, c.get(p2));
-}
-
 TEST(System, gemv) {
   // Points
   Set points;
