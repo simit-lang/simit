debee8cd2d84bca9243047834c9ed322f08d1da3
Gurtej Kanwar <gurtej@mit.edu>
2015-10-08 19:04:24 -0400

    Fixes error in single-precision pagerank test

--- test/program-pagerank-test.cpp
+++ test/program-pagerank-test.cpp
@@ -11,8 +11,8 @@ using namespace simit;
 TEST(Program, pagerank) {
   // Pages
   Set pages;
-  FieldRef<double> outlinks = pages.addField<double>("outlinks");
-  FieldRef<double> pr = pages.addField<double>("pr");
+  FieldRef<simit_float> outlinks = pages.addField<simit_float>("outlinks");
+  FieldRef<simit_float> pr = pages.addField<simit_float>("pr");
   
   // Links
   Set links(pages,pages);
