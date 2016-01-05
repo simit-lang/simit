5207ce0aa5b71490b519b1f7ff5a9e0289851959
Parker Tew <ptew@mit.edu>
2016-01-05 14:46:27 -0500

    first pass at refactoring reordering code. moved hilbert-reorder code to reorder src files

--- src/reorder.cpp
+++ src/reorder.cpp
@@ -2,15 +2,178 @@
 #include "reorder.h"
 
 #include "graph.h"
-#include "hilbert-reorder.h"
+#include "hilbert.h"
 #include <algorithm>
 #include <map>
 #include <vector>
 #include <list>
 #include <unordered_map>
 
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 namespace simit {
+  namespace hilbert {
+    // This function populates the hilbertId field of each vertex_t in nodes, by
+    // remapping every vertex onto an n^3 lattice using appropriate scaling factors,
+    // and then traversing the lattice using a 3-D Hilbert curve.
+    //
+    // hilbertBits = number of bits in Hilbert grid (grid with side 2^hilbertBits)
+    void assignHilbertIds(vertex_t * const nodes, const int cntNodes,
+                          const unsigned hilbertBits) {
+      // We first traverse all vertices to find the maximal and minimal coordinates
+      // along each axis: xMin, xMax, yMin, yMax, zMin, zMax.
+
+      double xMin, xMax, yMin, yMax, zMin, zMax;
+      uint64_t hilbertGridN = 1 << hilbertBits;
+
+      xMin = yMin = zMin = DBL_MAX;
+      xMax = yMax = zMax = DBL_MIN;
+      for (int i = 0; i < cntNodes; ++i) {
+        xMin = fmin(xMin, nodes[i].x);
+        yMin = fmin(yMin, nodes[i].y);
+        zMin = fmin(zMin, nodes[i].z);
+        xMax = fmax(xMax, nodes[i].x);
+        yMax = fmax(yMax, nodes[i].y);
+        zMax = fmax(zMax, nodes[i].z);
+      }
+
+      // We now create a mapping that maps:
+      //   (xMin, yMin, zMin) to lattice point (0, 0, 0)
+      //   (xMax, yMax, zMax) to lattice point (n-1, n-1, n-1)
+      // where n = hilbertGridN
+      //
+      // This mapping is the following: for the T axis,
+      // tLattice = round((t - tMin) * (hilbertGridN - 1) / tMax);
+
+      #pragma omp parallel for
+      for (int i = 0; i < cntNodes; ++i) {
+        bitmask_t latticeCoords[3];
+        bitmask_t hilbertIndex;
+
+        nodes[i].id = (vid_t)i;
+
+        latticeCoords[0] = (uint64_t) round((nodes[i].x - xMin) * (hilbertGridN - 1) / xMax);
+        latticeCoords[1] = (uint64_t) round((nodes[i].y - yMin) * (hilbertGridN - 1) / yMax);
+        latticeCoords[2] = (uint64_t) round((nodes[i].z - zMin) * (hilbertGridN - 1) / zMax);
+
+        hilbertIndex = hilbert_c2i(3, hilbertBits, latticeCoords);
+        nodes[i].hilbertId = (vid_t) hilbertIndex;
+
+      }
+    }
+
+    void bfs(vertex_t * const nodes, const int cntNodes, const vid_t source) {
+      for (int i = 0; i < cntNodes; i++) {
+        nodes[i].hilbertId = 0;
+      }
+
+      vid_t *bfs_q = new (std::nothrow) vid_t[cntNodes];
+      bfs_q[0] = source;
+      vid_t cur_q_ptr = 0;
+      nodes[source].hilbertId = 1;
+      vid_t cur_dist = 2;
+      vid_t next_batch_start_index = 1;
+      vid_t append_q_ptr = 1;
+
+      bool done = false;
+      while (!done) {
+        done = true;
+        while (cur_q_ptr != next_batch_start_index) {
+          vid_t vid = bfs_q[cur_q_ptr];
+          vid_t *edges = nodes[vid].edgeData.edges;
+          for (vid_t e = 0; e < nodes[vid].edgeData.cntEdges; e++) {
+            if (nodes[edges[e]].hilbertId == 0) {
+              nodes[edges[e]].hilbertId = cur_dist;
+              bfs_q[append_q_ptr++] = edges[e];
+            }
+          }
+          cur_q_ptr++;
+        }
+        // we added more nodes to the queue
+        if (next_batch_start_index < append_q_ptr) {
+          done = false;
+        }
+        // start of the next distance (from the BFS root) in the BFS queue
+        next_batch_start_index = append_q_ptr;
+        cur_dist++;
+      }
+
+      delete bfs_q;
+    }
+
+    void assignBfsIds(vertex_t * const nodes, const int cntNodes) {
+      bfs(nodes, cntNodes, 0);
+    }
+
+    void assignRandomIds(vertex_t * const nodes, const int cntNodes) {
+      vector<int> ordering;
+      for (int i = 0; i < cntNodes; i++) {
+        ordering.push_back(i);
+      }
+      random_shuffle(ordering.begin(), ordering.end());
+      for (int i = 0; i < cntNodes; i++) {
+        nodes[i].hilbertId = ordering[i];
+      }
+    }
+
+    bool vertexComparator(const vertex_t& a, const vertex_t& b) {
+      if (a.hilbertId != b.hilbertId) {
+        return a.hilbertId < b.hilbertId;
+      } else {
+        return a.id < b.id;
+      }
+    }
+
+    static void createIdTranslationMapping(vertex_t * reorderedNodes, vector<int>& vertexOrdering, int cntNodes) {
+      assert(vertexOrdering.size() != 0);
+      vertexOrdering.resize(cntNodes);
+
+      #pragma omp parallel for
+      for (int i = 0; i < cntNodes; ++i) {
+        vertexOrdering[reorderedNodes[i].id] = i;
+      }
+    }
+
+    void loadNodes(Set& vertexSet, vertex_t ** outNodes, int numNodes) {
+      vertex_t * nodes = new (std::nothrow) vertex_t[numNodes];
+      
+      auto& fields = vertexSet.getFields();
+      int fieldIndex = vertexSet.getFieldIndex(vertexSet.getSpatialFieldName());
+      double * spatialData = static_cast<double*>(fields[fieldIndex]->data);  
+     
+      for (int i = 0; i < numNodes; ++i) {
+        nodes[i].id = i; 
+        nodes[i].x = spatialData[i*3+0];
+        nodes[i].y = spatialData[i*3+1];
+        nodes[i].z = spatialData[i*3+2];
+      }
+      
+      *outNodes = nodes;
+    }
+
+    void hilbertReorder(Set& vertexSet, vector<int>& vertexOrdering, int cntNodes) {
+      vertex_t * nodes;
+      const int hilbertBits = 8;  
+      loadNodes(vertexSet, &nodes, cntNodes);
+
+      //assignBfsIds(nodes, cntNodes);
+      //assignRandomIds(nodes, cntNodes);
+      assignHilbertIds(nodes, cntNodes, hilbertBits);
+
+      stable_sort(nodes, nodes + cntNodes, vertexComparator);
+
+      createIdTranslationMapping(nodes, vertexOrdering, cntNodes);
+
+    }
+  } // namespace simit::hilbert
+  
   void bfs(vector<int>& vertexOrdering, int* endpoints, int size, int cardinality)
   {
     list<int> queue;
@@ -75,39 +238,6 @@ namespace simit {
     }
   }
 
-  int scoreEdges(const int* endpoints, const int size, const int cardinality) {
-    unordered_map<int,vector<int>> distances;
-    unordered_map<int,int> firstOccurrence;
-
-    for (int edgeIndex=0; edgeIndex < size * cardinality; edgeIndex += cardinality) {
-      for (int elementIndex=0; elementIndex < cardinality; ++elementIndex) {
-        auto search = firstOccurrence.find(endpoints[edgeIndex + elementIndex]);
-        if ( search != firstOccurrence.end() ) {
-          distances[endpoints[edgeIndex + elementIndex]].push_back( edgeIndex - search->second );
-        } else {
-          firstOccurrence[endpoints[edgeIndex + elementIndex]] = edgeIndex;
-          distances[endpoints[edgeIndex + elementIndex]] = vector<int>();
-        }
-      }
-    }
-
-    int distanceSum = 0;
-    for ( auto& distMapping : distances ) {
-      int distCount = distMapping.second.size();
-      for (int offset=0; offset < distCount - 1; ++offset) {
-        for (int first=offset; first < distCount - 1; ++first) {
-          for (int second=first + 1; second < distCount; ++second) {
-            iassert( first < second );
-            iassert( distMapping.second[first] < distMapping.second[second]);
-            distanceSum += distMapping.second[second] - distMapping.second[first];
-          }
-        }
-      }
-    }
-    
-    return distanceSum;
-  }
-  
   struct ascendingCompare {
       bool operator()(pair< int, int> const&left, 
                       pair< int, int> const&right) const {
@@ -167,7 +297,73 @@ namespace simit {
     }
   }
   
-  void reorderEdges(int* endpoints, const vector<int>& edgeOrdering, const int size, const int cardinality) {
+  // ---------- Ordering Evaluation Heuristics ----------
+  int scoreEdges(const int* endpoints, const int size, const int cardinality) {
+    unordered_map<int,vector<int>> distances;
+    unordered_map<int,int> firstOccurrence;
+
+    for (int edgeIndex=0; edgeIndex < size * cardinality; edgeIndex += cardinality) {
+      for (int elementIndex=0; elementIndex < cardinality; ++elementIndex) {
+        auto search = firstOccurrence.find(endpoints[edgeIndex + elementIndex]);
+        if ( search != firstOccurrence.end() ) {
+          distances[endpoints[edgeIndex + elementIndex]].push_back( edgeIndex - search->second );
+        } else {
+          firstOccurrence[endpoints[edgeIndex + elementIndex]] = edgeIndex;
+          distances[endpoints[edgeIndex + elementIndex]] = vector<int>();
+        }
+      }
+    }
+
+    int distanceSum = 0;
+    for ( auto& distMapping : distances ) {
+      int distCount = distMapping.second.size();
+      for (int offset=0; offset < distCount - 1; ++offset) {
+        for (int first=offset; first < distCount - 1; ++first) {
+          for (int second=first + 1; second < distCount; ++second) {
+            iassert( first < second );
+            iassert( distMapping.second[first] < distMapping.second[second]);
+            distanceSum += distMapping.second[second] - distMapping.second[first];
+          }
+        }
+      }
+    }
+    
+    return distanceSum;
+  }
+  
+  // ---------- Reordering Helper Functions ----------
+  
+  void reorderFields(vector<Set::FieldData*>& fields, const vector<int>& ordering) {
+    for (auto f : fields) {
+      switch (f->type->getComponentType()) {
+        case ComponentType::Float: {
+          float* data = static_cast<float *>(f->data);
+          reorderFieldData(data, ordering, f->sizeOfType);
+          break;
+        }
+        case ComponentType::Double: {
+          double* data = static_cast<double *>(f->data);
+          reorderFieldData(data, ordering, f->sizeOfType);
+          break;
+        }
+        case ComponentType::Int: {
+          int* data = static_cast<int *>(f->data);
+          reorderFieldData(data, ordering, f->sizeOfType);
+          break;
+        }
+        case ComponentType::Boolean: {
+          bool* data = static_cast<bool *>(f->data);
+          reorderFieldData(data, ordering, f->sizeOfType);
+        }
+      }
+    }
+  }
+  
+  void reorderEdgeSet(Set& edgeSet, const vector<int>& edgeOrdering) {
+    int* endpoints = edgeSet.getEndpointsPtr();
+    const int size = edgeSet.getSize();
+    const int cardinality = edgeSet.getCardinality();
+
     int* newEndpoints = static_cast<int *>(malloc(size * cardinality * sizeof(int)));
     memcpy(newEndpoints, endpoints, size * cardinality * sizeof(int));
 
@@ -178,22 +374,13 @@ namespace simit {
     }
     memcpy(endpoints, newEndpoints, size * cardinality * sizeof(int));
     delete newEndpoints;
+
+    assert(size == edgeOrdering.size());
+    
+    reorderFields(edgeSet.getFields(), edgeOrdering);
   }
 
-  void reorder(Set& edgeSet, Set& vertexSet, vector<int>& vertexOrdering) {
-    
-    //cout << "Score Pre Reorder" << endl;
-    //cout << "Total edge distance: " << scoreEdges(edgeSet.getEndpointsPtr(), getSize(), getCardinality()) << endl;
-    
-    // Get new vertex ordering based on given heuristic 
-    iassert(vertexOrdering.size() == 0);
-    //bfs(vertexOrdering, edgeSet.getEndpointsPtr(), vertexSet.getSize(), edgeSet.getCardinality());
-    hilbertReorder(vertexSet, vertexOrdering, vertexSet.getSize());
-    
-    //vertexDegreeReordering(vertexOrdering, edgeSet.getEndpointsPtr(), getSize(), getCardinality());
-    
-    iassert(vertexOrdering.size() == vertexSet.getSize()) << vertexOrdering.size() << ", " << vertexSet.getSize();
-    
+  void reorderVertexSet(Set& edgeSet, Set& vertexSet, vector<int>& vertexOrdering) {
     // Reset Endpoints to reflect reordering
     //    Vertex ordering maps old to new identity 
     //    This itertates over all enpoints 'translating' old to new
@@ -202,59 +389,22 @@ namespace simit {
       edgeSet.getEndpointsPtr()[i] = vertexOrdering[edgeSet.getEndpointsPtr()[i]]; 
     }
     
-    for (auto f : vertexSet.getFields()) {
-      switch (f->type->getComponentType()) {
-        case ComponentType::Float: {
-          float* data = static_cast<float *>(f->data);
-          reorderFieldData(data, vertexOrdering, vertexSet.getSize(), f->sizeOfType);
-          break;
-        }
-        case ComponentType::Double: {
-          double* data = static_cast<double *>(f->data);
-          reorderFieldData(data, vertexOrdering, vertexSet.getSize(), f->sizeOfType);
-          break;
-        }
-        case ComponentType::Int: {
-          int* data = static_cast<int *>(f->data);
-          reorderFieldData(data, vertexOrdering, vertexSet.getSize(), f->sizeOfType);
-          break;
-        }
-        case ComponentType::Boolean: {
-          bool* data = static_cast<bool *>(f->data);
-          reorderFieldData(data, vertexOrdering, vertexSet.getSize(), f->sizeOfType);
-        }
-      }
-    }
+    reorderFields(vertexSet.getFields(), vertexOrdering);
+  }
+  
+  void reorder(Set& edgeSet, Set& vertexSet, vector<int>& vertexOrdering) {
+    iassert(vertexOrdering.size() == 0);
     
+    // Get new vertex ordering based on given heuristic 
+    // bfs(vertexOrdering, edgeSet.getEndpointsPtr(), vertexSet.getSize(), edgeSet.getCardinality());
+    hilbert::hilbertReorder(vertexSet, vertexOrdering, vertexSet.getSize());
+    iassert(vertexOrdering.size() == vertexSet.getSize()) << vertexOrdering.size() << ", " << vertexSet.getSize();
+    reorderVertexSet(edgeSet, vertexSet, vertexOrdering);
+
     // vector<int> edgeOrdering;
-    // //cout << "Score before : " << scoreEdges(edgeSet.getEndpointsPtr(),edgeSet.getSize(), edgeSet.getCardinality()) << endl;
     // edgeSumReordering(edgeSet.getEndpointsPtr(), edgeOrdering, edgeSet.getSize(), edgeSet.getCardinality());
     // iassert(edgeOrdering.size() == edgeSet.getSize()); 
-    // reorderEdges(edgeSet.getEndpointsPtr(), edgeOrdering, edgeSet.getSize(), edgeSet.getCardinality()); 
-    // //cout << "Score after : " << scoreEdges(edgeSet.getEndpointsPtr(),edgeSet.getSize(), edgeSet.getCardinality()) << endl;
-    // 
-    // for (auto f : edgeSet.getFields()) {
-    //   switch (f->type->getComponentType()) {
-    //     case ComponentType::Float: {
-    //       float* data = static_cast<float *>(f->data);
-    //       reorderFieldData(data, edgeOrdering, edgeSet.getSize(), f->sizeOfType);
-    //       break;
-    //     }
-    //     case ComponentType::Double: {
-    //       double* data = static_cast<double *>(f->data);
-    //       reorderFieldData(data, edgeOrdering, edgeSet.getSize(), f->sizeOfType);
-    //       break;
-    //     }
-    //     case ComponentType::Int: {
-    //       int* data = static_cast<int *>(f->data);
-    //       reorderFieldData(data, edgeOrdering, edgeSet.getSize(), f->sizeOfType);
-    //       break;
-    //     }
-    //     case ComponentType::Boolean: {
-    //       bool* data = static_cast<bool *>(f->data);
-    //       reorderFieldData(data, edgeOrdering, edgeSet.getSize(), f->sizeOfType);
-    //     }
-    //   }
-    // }
+    // reorderEdgeSet(edgeSet, edgeOrdering);
   }
+
 }
--- src/reorder.h
+++ src/reorder.h
@@ -19,10 +19,6 @@ namespace simit {
     int fieldIndex = vertexSet.getFieldIndex("x");
     T* spatialData = static_cast<T*>(fields[fieldIndex]->data);
     
-    // std::ofstream output;
-    // output.open("/data/scratch/ptew/simit/test/input/program/fem/dragon-hilbert-edge-reorderd.ele");
-    // output << edgeset.getsize() << " 3 0 0\n"; 
-
     int dim = 3;
     int cardinality = edgeSet.getCardinality(); 
     std::vector<float> sum(3,0);
@@ -35,18 +31,18 @@ namespace simit {
       edgeSpatialField.set(element, sum);
       std::fill(sum.begin(), sum.end(), 0);
     }
-    // output.close();
   }
   
   template<typename T>
-  void reorderFieldData(T* data, const std::vector<int>& vertexOrdering, const int capacity, const int typeSize) {
+  void reorderFieldData(T* data, const std::vector<int>& vertexOrdering, const int typeSize) {
+    const int capacity = vertexOrdering.size(); 
     T* newData = static_cast<T*>(malloc(capacity * typeSize));
     int dim = typeSize/sizeof(T);
     assert(dim > 0);
 
-    for ( int i=0; i<vertexOrdering.size(); ++i) {
+    for ( int i=0; i < capacity; ++i) {
       for ( int x=0; x<dim; ++x) {
-        assert(vertexOrdering[i]*dim + x < vertexOrdering.size() * dim);
+        assert(vertexOrdering[i]*dim + x < capacity * dim);
         newData[vertexOrdering[i]*dim+x] = data[i*dim+x];
       }
     }
@@ -54,5 +50,32 @@ namespace simit {
     memcpy(data, newData, capacity * typeSize); 
     delete newData;
   }
+
+  namespace hilbert {
+    
+    typedef uint64_t vid_t;  // vertex id type
+
+    struct edges_t {
+      vid_t cntEdges;
+      vid_t * edges;
+    };
+    typedef struct edges_t edges_t;
+
+    struct vertex_t {
+      vid_t id;
+      vid_t hilbertId;
+      double x;
+      double y;
+      double z;
+      void * data;
+      edges_t edgeData;
+    };
+    typedef struct vertex_t vertex_t;
+
+    typedef std::pair<vid_t, vid_t> edge_t;
+
+    void hilbertReorder(Set& vertexSet, std::vector<int>& vertexOrdering, int vertexCount);
+  } // namespace simit::hilbert
+
 } // namespace simit 
 #endif
