e6af42393d5c3242c6b7fa3d1c285999f05455f5
Fredrik Kjolstad <fred@csail.mit.edu>
2016-08-12 16:27:59 -0700

    New assembly test suite

--- test/system-misc-tests.cpp
+++ test/system-misc-tests.cpp
@@ -135,39 +135,6 @@ TEST(system, map_assemble_from_literal_vector) {
   ASSERT_EQ(1.0, x.get(p1));
 }
 
-TEST(system, map_assemble_vector_components) {
-  // Points
-  Set points;
-  FieldRef<simit_float,2> x = points.addField<simit_float,2>("x");
-
-  ElementRef p0 = points.add();
-  ElementRef p1 = points.add();
-  ElementRef p2 = points.add();
-
-  // Springs
-  Set springs(points,points);
-
-  springs.add(p0,p1);
-  springs.add(p1,p2);
-
-  // Compile program and bind arguments
-  Function func = loadFunction(TEST_FILE_NAME, "main");
-  if (!func.defined()) FAIL();
-
-  func.bind("points", &points);
-  func.bind("springs", &springs);
-
-  func.runSafe();
-
-  // Check that outputs are correct
-  ASSERT_EQ(42.0, x.get(p0)(0));
-  ASSERT_EQ(0.0,  x.get(p0)(1));
-  ASSERT_EQ(42.0,  x.get(p1)(0));
-  ASSERT_EQ(0.0, x.get(p1)(1));
-  ASSERT_EQ(0.0,  x.get(p2)(0));
-  ASSERT_EQ(0.0, x.get(p2)(1));
-}
-
 TEST(system, map_assemble_blocked) {
   // Points
   Set points;
@@ -196,65 +163,6 @@ TEST(system, map_assemble_blocked) {
   ASSERT_EQ(729.0, z.get(p2));
 }
 
-TEST(system, DISABLED_map_assemble_fem) {
-  // Points
-  Set points;
-  FieldRef<simit_float> x = points.addField<simit_float>("x");
-
-  ElementRef p0 = points.add();
-  ElementRef p1 = points.add();
-  ElementRef p2 = points.add();
-
-  x.set(p0, 1.0);
-  x.set(p1, 1.0);
-
-  // Springs
-  Set springs(points,points);
-  FieldRef<simit_float> u = springs.addField<simit_float>("u");
-
-  springs.add(p0,p1);
-  springs.add(p1,p2);
-
-  // Compile program and bind arguments
-  Function func = loadFunction(TEST_FILE_NAME, "main");
-  if (!func.defined()) FAIL();
-
-  func.bind("points", &points);
-  func.bind("springs", &springs);
-
-  func.runSafe();
-
-  // Check that outputs are correct
-  ASSERT_EQ(1.0, x.get(p0));
-  ASSERT_EQ(1.0, x.get(p1));
-}
-
-TEST(system, map_one_set) {
-  // Points
-  Set points;
-  FieldRef<simit_float> a = points.addField<simit_float>("a");
-
-  ElementRef p0 = points.add();
-  ElementRef p1 = points.add();
-  ElementRef p2 = points.add();
-
-  a.set(p0, 1.0);
-  a.set(p1, 2.0);
-  a.set(p2, 3.0);
-
-  // Compile program and bind arguments
-  Function func = loadFunction(TEST_FILE_NAME, "main");
-  if (!func.defined()) FAIL();
-
-  func.bind("points", &points);
-  func.runSafe();
-
-  // Check outputs
-  SIMIT_ASSERT_FLOAT_EQ(2, a.get(p0));
-  SIMIT_ASSERT_FLOAT_EQ(4, a.get(p1));
-  SIMIT_ASSERT_FLOAT_EQ(6, a.get(p2));
-}
-
 TEST(system, map_env_folding) {
   // Points
   Set points;
@@ -279,32 +187,6 @@ TEST(system, map_env_folding) {
   SIMIT_ASSERT_FLOAT_EQ(5.0, a.get(p0));
   SIMIT_ASSERT_FLOAT_EQ(5.0, a.get(p1));
   SIMIT_ASSERT_FLOAT_EQ(5.0, a.get(p2));
-}
-
-TEST(system, DISABLED_map_one_set_const_ref) {
-  // Points
-  Set points;
-  FieldRef<simit_float> a = points.addField<simit_float>("a");
-
-  ElementRef p0 = points.add();
-  ElementRef p1 = points.add();
-  ElementRef p2 = points.add();
-
-  a.set(p0, 1.0);
-  a.set(p1, 2.0);
-  a.set(p2, 3.0);
-
-  // Compile program and bind arguments
-  Function func = loadFunction(TEST_FILE_NAME, "main");
-  if (!func.defined()) FAIL();
-
-  func.bind("points", &points);
-  func.runSafe();
-
-  // Check outputs
-  SIMIT_ASSERT_FLOAT_EQ(0.5, a.get(p0));
-  SIMIT_ASSERT_FLOAT_EQ(1.0, a.get(p1));
-  SIMIT_ASSERT_FLOAT_EQ(1.5, a.get(p2));
 }
 
 TEST(system, map_no_results_one_set) {
