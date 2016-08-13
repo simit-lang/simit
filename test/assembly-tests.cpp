51cd8e463aa710fda958c7e95892144d609de966
Fredrik Kjolstad <fred@csail.mit.edu>
2016-08-13 14:53:57 -0700

    Moving tests to assembly and apply suites.
    Also cleaning up test code and deleting some tests.

--- test/assembly-tests.cpp
+++ test/assembly-tests.cpp
@@ -16,18 +16,63 @@ TEST(assembly, vertices) {
   ElementRef v2 = V.add();
   FieldRef<int> a = V.addField<int>("a");
   FieldRef<int> b = V.addField<int>("b");
-  a.set(v0, 1);
-  a.set(v1, 2);
-  a.set(v2, 3);
+  a(v0) = 1;
+  a(v1) = 2;
+  a(v2) = 3;
 
   Function func = loadFunction(TEST_FILE_NAME, "main");
   if (!func.defined()) FAIL();
   func.bind("V", &V);
   func.runSafe();
 
-  ASSERT_EQ(2, b.get(v0));
-  ASSERT_EQ(4, b.get(v1));
-  ASSERT_EQ(6, b.get(v2));
+  ASSERT_EQ(2, b(v0));
+  ASSERT_EQ(4, b(v1));
+  ASSERT_EQ(6, b(v2));
+}
+
+TEST(assembly, vertices_two_results) {
+  Set V;
+  ElementRef v0 = V.add();
+  ElementRef v1 = V.add();
+  ElementRef v2 = V.add();
+  FieldRef<int> a = V.addField<int>("a");
+  FieldRef<int> b = V.addField<int>("b");
+  a(v0) = 1;
+  a(v1) = 2;
+  a(v2) = 3;
+
+  Function func = loadFunction(TEST_FILE_NAME, "main");
+  if (!func.defined()) FAIL();
+  func.bind("V", &V);
+  func.runSafe();
+
+  ASSERT_EQ( 6, (int)b(v0));
+  ASSERT_EQ(24, (int)b(v1));
+  ASSERT_EQ(54, (int)b(v2));
+}
+
+TEST(assembly, edges_no_endpoints) {
+  Set V;
+  ElementRef v0 = V.add();
+  ElementRef v1 = V.add();
+  ElementRef v2 = V.add();
+
+  Set E(V,V);
+  ElementRef e0 = E.add(v0,v1);
+  ElementRef e1 = E.add(v1,v2);
+  FieldRef<int> a = E.addField<int>("a");
+  FieldRef<int> b = E.addField<int>("b");
+  a(e0) = 1;
+  a(e1) = 2;
+
+  Function func = loadFunction(TEST_FILE_NAME, "main");
+  if (!func.defined()) FAIL();
+  func.bind("V", &V);
+  func.bind("E", &E);
+  func.runSafe();
+
+  ASSERT_EQ(2, (int)b(e0));
+  ASSERT_EQ(4, (int)b(e1));
 }
 
 TEST(assembly, edges_unary) {
@@ -74,6 +119,58 @@ TEST(assembly, edges_binary) {
   ASSERT_EQ((int)a(v2), 1);
 }
 
+TEST(assembly, edges_tertiary) {
+  Set V;
+  ElementRef v0 = V.add();
+  ElementRef v1 = V.add();
+  ElementRef v2 = V.add();
+  ElementRef v3 = V.add();
+  FieldRef<int> b = V.addField<int>("b");
+
+  Set E(V,V,V);
+  ElementRef e0 = E.add(v0,v1,v2);
+  ElementRef e1 = E.add(v1,v2,v3);
+  simit::FieldRef<int> a = E.addField<int>("a");
+  a(e0) = 10;
+  a(e1) = 1;
+
+  Function func = loadFunction(TEST_FILE_NAME, "main");
+  if (!func.defined()) FAIL();
+  func.bind("V", &V);
+  func.bind("E", &E);
+  func.runSafe();
+
+  ASSERT_EQ(10, b(v0));
+  ASSERT_EQ(11, b(v1));
+  ASSERT_EQ(11, b(v2));
+  ASSERT_EQ(1,  b(v3));
+}
+
+TEST(assembly, edges_two_results) {
+  Set V;
+  ElementRef v0 = V.add();
+  ElementRef v1 = V.add();
+  ElementRef v2 = V.add();
+  FieldRef<int> b = V.addField<int>("b");
+
+  Set E(V,V);
+  ElementRef e0 = E.add(v0,v1);
+  ElementRef e1 = E.add(v1,v2);
+  FieldRef<int> a = E.addField<int>("a");
+  a(e0) = 1;
+  a(e1) = 2;
+
+  Function func = loadFunction(TEST_FILE_NAME, "main");
+  if (!func.defined()) FAIL();
+  func.bind("V", &V);
+  func.bind("E", &E);
+  func.runSafe();
+
+  ASSERT_EQ(43,  (int)b(v0));
+  ASSERT_EQ(177, (int)b(v1));
+  ASSERT_EQ(168, (int)b(v2));
+}
+
 TEST(DISABLED_assembly, matrix_rectangular) {
   Set V;
   ElementRef v0 = V.add();
@@ -99,6 +196,27 @@ TEST(DISABLED_assembly, matrix_rectangular) {
   ASSERT_EQ((int)a(e1), 5);
 }
 
+TEST(assembly, blocked) {
+  Set V;
+  ElementRef v0 = V.add();
+  ElementRef v1 = V.add();
+  ElementRef v2 = V.add();
+  FieldRef<int> x = V.addField<int>("x");
+  FieldRef<int> z = V.addField<int>("z");
+  x(v0) = 1;
+  x(v1) = 2;
+  x(v2) = 3;
+
+  Function func = loadFunction(TEST_FILE_NAME, "main");
+  if (!func.defined()) FAIL();
+  func.bind("V", &V);
+  func.runSafe();
+
+  ASSERT_EQ(81,  z(v0));
+  ASSERT_EQ(324, z(v1));
+  ASSERT_EQ(729, z(v2));
+}
+
 TEST(assembly, block_component_write) {
   Set V;
   ElementRef v0 = V.add();
@@ -116,10 +234,10 @@ TEST(assembly, block_component_write) {
   func.bind("E", &E);
   func.runSafe();
 
-  ASSERT_EQ(1, x.get(v0)(0));
-  ASSERT_EQ(0, x.get(v0)(1));
-  ASSERT_EQ(1, x.get(v1)(0));
-  ASSERT_EQ(0, x.get(v1)(1));
-  ASSERT_EQ(0, x.get(v2)(0));
-  ASSERT_EQ(0, x.get(v2)(1));
+  ASSERT_EQ(1, x(v0)(0));
+  ASSERT_EQ(0, x(v0)(1));
+  ASSERT_EQ(1, x(v1)(0));
+  ASSERT_EQ(0, x(v1)(1));
+  ASSERT_EQ(0, x(v2)(0));
+  ASSERT_EQ(0, x(v2)(1));
 }
