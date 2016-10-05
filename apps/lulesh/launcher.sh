a09bc0439e171851293c5c9750f4f708811cbfcc
David Lugato <david.lugato@gmail.com>
2016-10-05 18:00:54 -0400

    light adjustments on lulesh

--- apps/lulesh/launcher.sh
+++ apps/lulesh/launcher.sh
@@ -1,11 +1,23 @@
 
 # small script to launch several instances of lulesh-simit and get time results
 
-launchs=$1
+version=$1
+launchs=$2
+#size=$3
+iters=$3
+
+cd build
+
+for size in {150..150}
+do
+echo "Version : "$version" launchs : "$launchs" size : "$size" iters : "$iters
+
 for i in $(eval echo "{1..$launchs}")
    do
-    ./build/lulesh-simit > tmpTime
+	./lulesh-$version -s $size -i $iters -r 1 > tmpTime
     grep Elapsed tmpTime | cut -d "=" -f 2 | cut -f 1 -d "("
 done
 rm tmpTime
+done
+cd -
 
